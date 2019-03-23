Bug Fixes to Interference Calculation Module

dist_check.py
@@ -9,6 +9,7 @@
 # ======================
 
 from bp_assister import bp_assist
+import copy
 
 # ===========================
 # Inter-site distance checker
@@ -131,22 +132,25 @@ def idx_mat(src_mat, param_val, srch_type, np): # This function works as an elem
 # ==============================
 
 def interf(PL, scn, np): # This function returns the overall interference matrix given a Pathloss matrix
-
     interf = np.empty((PL.shape[0],PL.shape[1])); # Initialize the interference matrix
     PR_interf = interf; # This is a temporary matrix to hold Rx Power values due to all other APs other than AP of interest
-    print PL[1,:]
-    print "Next"
+    #print PL[1,:]
+    #print "Next"
     for i in range(0, PL.shape[1]):
-        PL_temp = PL; # This is a temporary array store
+        PL_temp = copy.copy(PL); # This is a temporary array store
         PL_temp[:,i] = float('nan'); # So the array now has Nan where we have our AP of interest
-        for j in range(0, PL.shape[0]):
-            PR_interf[j,i] = np.where(np.isnan(PL_temp[j,i]), float('nan'), (10**(scn.transmit_power/10)*(10**(scn.transmit_gain_sc/10))*(10**(scn.receiver_gain/10)*10**(-3)))/(10**(PL[j,i]/10))); # PL for interference matrix
-        for j in range(0, PL.shape[0]):
-            if np.isnan(PL[j,i]):
-                interf[j,i] = 0; # Interference power is 0 if the SC is not under consideration
-            else:
-                interf[j,i] = np.sum(PR_interf[j, np.where(np.isnan(PR_interf[j,:]) != True)], axis=1)   
-    print PR_interf[1,:]
+        print PL_temp[1,:]
+        print "Next"
+        PR_interf = (10**(scn.transmit_power/10)*(10**(scn.transmit_gain_sc/10))*(10**(scn.receiver_gain/10)*10**(-3)))/(10**(PL_temp/10)); # Compute the received power on each UE-AP pair
+        interf[:,i] = np.sum(np.where(np.isnan(PR_interf), 0, PR_interf), axis=1); #Interference from other APs for a given UE-AP pair
+        #for j in range(0, PL.shape[0]):
+        #    PR_interf[j,:] = np.where(np.isnan(PL_temp[j,:]), float('nan'), (10**(scn.transmit_power/10)*(10**(scn.transmit_gain_sc/10))*(10**(scn.receiver_gain/10)*10**(-3)))/(10**(PL[j,:]/10))); # PL for interference matrix
+        #for j in range(0, PL.shape[0]):
+         #   if np.isnan(PL[j,i]):
+        #        interf[j,i] = 0; # Interference power is 0 if the SC is not under consideration
+        #    else:
+        #        interf[j,i] = np.sum(PR_interf[j, np.where(np.isnan(PR_interf[j,:]) != True)], axis=1)   
+    print interf[1,:]
     return interf
 
     

scenario_gen.py
@@ -145,13 +145,13 @@ def sinr_gen (scn, num_SCBS, mc_locs, sc_locs, usr_locs_eMBB, usr_locs_URLLC, us
                 PL_sc[i,j] = pathloss.pathloss_SC(scn, sorted_SCBS_eMBB_mat[i][j], np, dist_serv_sc_eMBB_3d[i][j], dsc); # Calculating the pathloss for Small cells
                 #snr_sc[i][j] = scn.transmit_power + scn.transmit_gain_sc + scn.receiver_gain - PL_sc - (scn.N + 10*np.log10(scn.sc_bw)); # This is the SNR from one Small cell 
             else:
-                PL_sc[i,j] = None; # Nan for no PL calc
+                PL_sc[i,j] = float('nan'); # Nan for no PL calc
 
     # ========================
     # Interference Calculation
 
     interf_sc = dsc.interf(PL_sc, scn, np); # Calulate the interference matrix
-    print interf_sc[1,:]
+    #print interf_sc[1,:]
 
     # SINR Calculation
 