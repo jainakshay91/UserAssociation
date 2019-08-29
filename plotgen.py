475540c2867bbc6047ac8d7833eb3a7a83a069bb
jainakshay91 <akshay1991jain@gmail.com>
2019-08-29 14:41:22 +0200

    Plot Generation for Split Graphs

--- plotgen.py
+++ plotgen.py
@@ -603,43 +603,43 @@ print ('DC+MRT+LAT Accepted Users:',AU_DR_DC_MRT_LAT_avg)
 
 # ===> Setting up a Broken plot to depict Values with distinct ranges
 
-#f, (ax, ax2) = plt.subplots(2, 1, sharex = True)
+f, (ax, ax2) = plt.subplots(2, 1, sharex = True)
 
 # plot the same data on both axes
-#ax.plot(x_axis, Net_Throughput_avg, 'r--*', x_axis, Net_Throughput_DC_avg, 'b--*' , x_axis, Net_Throughput_DC_MRT_avg, 'g-.', x_axis, Net_Throughput_DC_BHCAP_avg, 'k--s', x_axis, Net_Throughput_DC_BHCAP_LAT_avg, 'm--d', x_axis , Net_Throughput_DC_LAT_avg, 'c--p',x_axis, Net_Throughput_SA_MRT_avg, 'k-.', x_axis, Net_Throughput_SA_LAT_avg, 'b:', x_axis, Net_Throughput_SA_BHCAP_avg, 'g--D', x_axis, Net_Throughput_SA_BHCAP_LAT_avg, 'r:', x_axis, Net_Throughput_SA_MRT_LAT_avg, 'r-^', x_axis, Net_Throughput_DC_MRT_LAT_avg, 'k-o')
-#ax2.plot(x_axis, B_Dat_DR_avg, 'k--x') #x_axis, B_Dat_DR_sn_avg, 'b--x', 
+ax.plot(x_axis, Net_Throughput_avg, 'r--*', x_axis, Net_Throughput_DC_avg, 'b--*' , x_axis, Net_Throughput_DC_MRT_avg, 'g-.', x_axis, Net_Throughput_DC_BHCAP_avg, 'k--s', x_axis, Net_Throughput_DC_BHCAP_LAT_avg, 'm--d', x_axis , Net_Throughput_DC_LAT_avg, 'c--p',x_axis, Net_Throughput_SA_MRT_avg, 'k-.', x_axis, Net_Throughput_SA_LAT_avg, 'b:', x_axis, Net_Throughput_SA_BHCAP_avg, 'g--D', x_axis, Net_Throughput_SA_BHCAP_LAT_avg, 'r:', x_axis, Net_Throughput_SA_MRT_LAT_avg, 'r-^', x_axis, Net_Throughput_DC_MRT_LAT_avg, 'k-o')
+ax2.plot(x_axis, B_Dat_DR_avg, 'k--x') #x_axis, B_Dat_DR_sn_avg, 'b--x', 
 
-#ax2.set_ylim(y_min_2,y_max_2 + 0.5*1e8)
-#ax2.set_yticks((y_min_2,y_max_2))
-#ax.set_ylim(y_min_1,y_max_1+1e10)
-#ax.set_yticks((y_min_1,y_max_1,1e10))
+ax2.set_ylim(y_min_2,y_max_2 + 0.5*1e8)
+ax2.set_yticks((y_min_2,y_max_2))
+ax.set_ylim(y_min_1,y_max_1+1e10)
+ax.set_yticks((y_min_1,y_max_1,1e10))
 
-#ax.spines['bottom'].set_visible(False)
-#ax2.spines['top'].set_visible(False)
-#ax.xaxis.tick_top()
-#ax.tick_params(labeltop='off')
-#ax2.xaxis.tick_bottom()
+ax.spines['bottom'].set_visible(False)
+ax2.spines['top'].set_visible(False)
+ax.xaxis.tick_top()
+ax.tick_params(labeltop='off')
+ax2.xaxis.tick_bottom()
 
-#f.legend(['Single Association (SA)','Dual Connectivity (DC)', 'DC + Minimum Rate', 'DC + Constrained Backhaul (CB) [1% Bound Gap]', 'DC + CB + Constrained Path Latency (CPL) [1% Bound Gap]', 'DC + CPL', 'SA + Minimum Rate', 'SA + CPL', 'SA + CB [1% Bound Gap]', 'SA + CB + CPL [1% Bound Gap]','SA + Minimum Rate + CPL', 'DC + Minimum Rate + CPL', 'Baseline(SINR)'], loc='upper right', bbox_to_anchor=(0., 0.5, 0.83, 0.18), prop={'size': 5.5}) #'Baseline (RSSI)',
+f.legend(['Single Association (SA)','Dual Connectivity (DC)', 'DC + Minimum Rate', 'DC + Constrained Backhaul (CB) [1% Bound Gap]', 'DC + CB + Constrained Path Latency (CPL) [1% Bound Gap]', 'DC + CPL', 'SA + Minimum Rate', 'SA + CPL', 'SA + CB [1% Bound Gap]', 'SA + CB + CPL [1% Bound Gap]','SA + Minimum Rate + CPL', 'DC + Minimum Rate + CPL', 'Baseline(SINR)'], loc='upper right', bbox_to_anchor=(0., 0.5, 0.83, 0.18), prop={'size': 5.5}) #'Baseline (RSSI)',
 
-#d = .015  # how big to make the diagonal lines in axes coordinates
+d = .015  # how big to make the diagonal lines in axes coordinates
 # arguments to pass to plot, just so we don't keep repeating them
-#kwargs = dict(transform=ax.transAxes, color='k', clip_on=False)
-#ax.plot((-d, +d), (-d, +d), **kwargs)        # top-left diagonal
-#ax.plot((1 - d, 1 + d), (-d, +d), **kwargs)  # top-right diagonal
+kwargs = dict(transform=ax.transAxes, color='k', clip_on=False)
+ax.plot((-d, +d), (-d, +d), **kwargs)        # top-left diagonal
+ax.plot((1 - d, 1 + d), (-d, +d), **kwargs)  # top-right diagonal
 
-#kwargs.update(transform=ax2.transAxes)  # switch to the bottom axes
-#ax2.plot((-d, +d), (1 - d, 1 + d), **kwargs)  # bottom-left diagonal
-#ax2.plot((1 - d, 1 + d), (1 - d, 1 + d), **kwargs)  # bottom-right diagonal
+kwargs.update(transform=ax2.transAxes)  # switch to the bottom axes
+ax2.plot((-d, +d), (1 - d, 1 + d), **kwargs)  # bottom-left diagonal
+ax2.plot((1 - d, 1 + d), (1 - d, 1 + d), **kwargs)  # bottom-right diagonal
 
 #plt.plot(x_axis, B_Dat_DR_avg, 'b-', x_axis, B_Dat_DR_sn_avg, 'r-')
-#ax.grid(which= 'both',axis= 'both')
-#ax2.grid(which= 'both',axis= 'both')
-#f.suptitle('Total Network Throughput (User Bandwidth=200 MHz)')
-#plt.savefig('NetThrough', dpi=1200, facecolor='w', edgecolor='w',
-#        orientation='landscape', papertype='letter', format='png',
-#        transparent=False, bbox_inches='tight', pad_inches=0.1,
-#        frameon=None, metadata=None)
+ax.grid(which= 'both',axis= 'both')
+ax2.grid(which= 'both',axis= 'both')
+f.suptitle('Total Network Throughput (User Bandwidth=200 MHz)')
+plt.savefig('NetThrough_Split', dpi=1200, facecolor='w', edgecolor='w',
+        orientation='landscape', papertype='letter', format='png',
+        transparent=False, bbox_inches='tight', pad_inches=0.1,
+        frameon=None, metadata=None)
 
 # ================
 # Fairness BoxPlot
