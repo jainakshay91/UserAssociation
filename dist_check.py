Bug Fixes. MCBS Sectorization. SCBS BF. Updated Interference Calc. IF Limited and BF regime simulation. Updated LOS-NLOS Calculation. Updated Plotter.

dist_check.py
@@ -152,8 +152,11 @@ def dist_calc(locs_src, locs_tgt, usr_ht, bs_ht, dist_type, np):
 def idx_mat(src_mat, param_val, srch_type, np): # This function works as an element locator and distance based Sorter
    
     if srch_type == 'minimum':
+        #print src_mat
         sorted_mat = np.sort(src_mat,kind='mergesort')[:,:param_val]; # Sort the matrix first
+        #print sorted_mat
         sorted_idx = np.argsort(src_mat,kind='mergesort')[:,:param_val]; #Indices of the sorted matrix
+        #print sorted_idx
         return sorted_mat,sorted_idx # Returning the sorted matrix and the index of the requested elements in the original matrix
    
     # This function can be extended further for maximum or non-maximal/minimal scenarios
@@ -166,11 +169,11 @@ def idx_mat(src_mat, param_val, srch_type, np): # This function works as an elem
         #print sorted_idx[1]
         return np.where(sorted_mat>200,0,sorted_mat),sorted_idx # Return Sorted Matrix and the indices 
     
-# ==============================
-# Interference Matrix Calculator
-# ==============================
+# ============================================================
+# Interference Limited Scenario Interference Matrix Calculator
+# ============================================================
 
-def interf(PL, scn, np): # This function returns the overall interference matrix given a Pathloss matrix
+def interf(PL, scn, np, tx_power, gain): # This function returns the overall interference matrix given a Pathloss matrix
     interf = np.empty((PL.shape[0],PL.shape[1])); # Initialize the interference matrix
     PR_interf = interf; # This is a temporary matrix to hold Rx Power values due to all other APs other than AP of interest
     #print PL[1,:]
@@ -178,9 +181,49 @@ def interf(PL, scn, np): # This function returns the overall interference matrix
     for i in range(0, PL.shape[1]):
         PL_temp = copy.copy(PL); # This is a temporary array store
         PL_temp[:,i] = float('nan'); # So the array now has Nan where we have our AP of interest
-        PR_interf = (10**(scn.transmit_power/10)*(10**(scn.transmit_gain_sc/10))*(10**(scn.receiver_gain/10)*10**(-3)))/(10**(PL_temp/10)); # Compute the received power on each UE-AP pair
+        PR_interf = (10**(tx_power/10)*(10**(gain/10))*(10**(scn.receiver_gain/10)*10**(-3)))/(10**(PL_temp/10)); # Compute the received power on each UE-AP pair
         interf[:,i] = np.sum(np.where(np.isnan(PR_interf), 0, PR_interf), axis=1); #Interference from other APs for a given UE-AP pair
     return interf
+
+# ===========================================================
+# Small Cell Beamforming Based Interference Matrix Calculator
+# ===========================================================
+ 
+def angsc(usr_loc, sc_loc, np, scn): # This function will find the beam that points in the direction of the user
+    
+    beam_angles = np.arange(scn.beam_hpbw_rx, 360 + scn.beam_hpbw_rx , scn.beam_hpbw_rx) # UE RX beam angles
+    #print beam_angles
+
+
+    coord_diff = usr_loc - sc_loc # Compute the Perpendicular
+    theta_diff = np.degrees(np.arctan(coord_diff[1]/coord_diff[0])) # Computes the vector of angles each UE makes with a MCBS
+    #print theta_diff
+
+
+    theta_diff = np.where(theta_diff >= 0 , theta_diff, 360 + theta_diff)
+    #print theta_diff
+
+
+
+    angle_diff = beam_angles - theta_diff; # Subtract the angle of the current AP of interest with the beam sectors
+    #print angle_diff
+
+    sect_ang = np.where(angle_diff>=0)[0][0]; # Stores the index where a given AP is located within the UE beam
+    
+    #print sect_ang
+    return int(sect_ang)
+
+# def interf_sect(interf, idx_curr_MCBS, scn, np): # This function returns the overall interference matrix given a Pathloss matrix and the sectorization matrix
+#     interf = np.empty((PL.shape[0],PL.shape[1])); # Initialize the interference matrix
+#     PR_interf = interf; # This is a temporary matrix to hold Rx Power values due to all other APs other than AP of interest
+#     #print PL[1,:]
+#     #print "Next"
+#     for i in range(0, PL.shape[1]):
+#         PL_temp = copy.copy(PL); # This is a temporary array store
+#         PL_temp[:,i] = float('nan'); # So the array now has Nan where we have our AP of interest
+#         PR_interf = (10**(scn.transmit_power/10)*(10**(scn.transmit_gain_sc/10))*(10**(scn.receiver_gain/10)*10**(-3)))/(10**(PL_temp/10)); # Compute the received power on each UE-AP pair
+#         interf[:,i] = np.sum(np.where(np.isnan(PR_interf), 0, PR_interf), axis=1); #Interference from other APs for a given UE-AP pair
+#     return interf
 
 # ==================
 # Matrix Reorganizer

los_probability.py
@@ -2,36 +2,75 @@
 # LOS Probability Generator
 # =========================
 
+import dist_check as dsc
+from scenario_var import scenario_var 
+
 # ==================================================
 # Los Probability for UMi Street Canyon (Small Cell)
 
-def los_prob_sc (np,dist):
-    # We take the expected value, i.e. a value over a 1000 iterations
-    los_prob = 0; # Probability holder variable
-    if dist<=18:
-        los_prob = 1; # The UE is in a LOS zone surely given that the distance is less than 18m
-        return los_prob # Return the los probability
-    else:
-        for i in range(0,1000):
-            intr_prob = (18/dist)*(np.exp(-1*(dist/36)))*(1-18/dist); # Probability of a UE being in LOS zone given that the distance is beyond 18m
-            los_prob = los_prob + intr_prob; # We sum and average to obtain the average probability value   
-        return los_prob/i # We return the expected value of LOS Probability
+
+scn = scenario_var();
+def los_prob(np, dist,sc_flag):
+    if sc_flag == 1: # Small Cell
+        los_prob = 0; # Probability holder variable
+        if dist<=18:
+            los_prob = 1; # The UE is in a LOS zone surely given that the distance is less than 18m
+            #print ("SC within dist:", los_prob)
+            return los_prob # Return the los probability
+        else:
+            for i in range(0,1000):
+                intr_prob = (18/dist)*(np.exp(-1*(dist/36)))*(1-18/dist); # Probability of a UE being in LOS zone given that the distance is beyond 18m
+                los_prob = los_prob + intr_prob; # We sum and average to obtain the average probability value   
+            #print ("Small Cell LOS Probability", los_prob/i)
+            return los_prob/i # We return the expected value of LOS Probability
+
+    elif sc_flag == 0: # Macro Cell
+        los_prob = 0; # Los probability holder variable
+        C = dsc.los_prob_var_gen(scn.usr_ht); # Compute the Necessary Variables
+        if dist<=18:
+            los_prob = 1; # The UE is in LOS always given that the distance is less than 18m
+            #print ("MC within dist:", los_prob)
+            return los_prob
+        else: 
+            for i in range(0,1000):
+                intr_prob = (18/dist + np.exp(-1*dist/63)*(1-18/dist))*(1 + C*(5/4)*((dist/100)**3)*np.exp(-1*dist/150)); # Probability of a UE being in LOS given that the distance between UE and MC is more than 18m
+                los_prob = los_prob + intr_prob
+            #print ("Macro Cell LOS Probability",los_prob/i )
+            return los_prob/i # Return the average which will be close to the expected value         
 
 
-# ==============================================================
-# Los Probability for the UMa (Macro Cell in Urban Environments)
 
-def los_prob_mc(np,dist,C):
-    # We take the expected value, i.e. an average over 1000 iterations
-    los_prob = 0; # Los probability holder variable
-    if dist<=18:
-        los_prob = 1; # The UE is in LOS always given that the distance is less than 18m
-        return los_prob
-    else: 
-        for i in range(0,1000):
-            intr_prob = (18/dist + np.exp(-1*dist/63)*(1-18/dist))*(1 + C*(5/4)*((dist/100)**3)*np.exp(-1*dist/150)); # Probability of a UE being in LOS given that the distance between UE and MC is more than 18m
-            los_prob = los_prob + intr_prob
-        return los_prob/i # Return the average which will be close to the expected value 
+# def los_prob_sc (np,dist):
+#     # We take the expected value, i.e. a value over a 1000 iterations
+#     los_prob = 0; # Probability holder variable
+#     if dist<=18:
+#         los_prob = 1; # The UE is in a LOS zone surely given that the distance is less than 18m
+#         print ("SC within dist:", los_prob)
+#         return los_prob # Return the los probability
+#     else:
+#         for i in range(0,1000):
+#             intr_prob = (18/dist)*(np.exp(-1*(dist/36)))*(1-18/dist); # Probability of a UE being in LOS zone given that the distance is beyond 18m
+#             los_prob = los_prob + intr_prob; # We sum and average to obtain the average probability value   
+#         print ("Small Cell LOS Probability", los_prob/i)
+#         return los_prob/i # We return the expected value of LOS Probability
+
+
+# # ==============================================================
+# # Los Probability for the UMa (Macro Cell in Urban Environments)
+
+# def los_prob_mc(np,dist,C):
+#     # We take the expected value, i.e. an average over 1000 iterations
+#     los_prob = 0; # Los probability holder variable
+#     if dist<=18:
+#         los_prob = 1; # The UE is in LOS always given that the distance is less than 18m
+#         #print ("MC within dist:", los_prob)
+#         return los_prob
+#     else: 
+#         for i in range(0,1000):
+#             intr_prob = (18/dist + np.exp(-1*dist/63)*(1-18/dist))*(1 + C*(5/4)*((dist/100)**3)*np.exp(-1*dist/150)); # Probability of a UE being in LOS given that the distance between UE and MC is more than 18m
+#             los_prob = los_prob + intr_prob
+#         #print ("Macro Cell LOS Probability",los_prob/i )
+#         return los_prob/i # Return the average which will be close to the expected value 
 
 
 

main.py
@@ -19,6 +19,7 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser(description = 'Scenario Generator Main Function'); # Initializing the class variable
 parser.add_argument('-iter', type = int, help = 'Iteration Number of the Simulation');
+parser.add_argument('-interf', type = int, help = 'Interference Limited Region Indicator')
 args = parser.parse_args(); # Parse the Arguments
 
 # =====================================
@@ -109,14 +110,14 @@ try:
 	for i in range(0,len(usr_locs.keys())):
 		print "Iteration #" + str(i)
 		print "====================="
-		sinr_sorted, locs_sc_ret, usr_lcs, idx_sc, idx_mc, sinr_pad, num_SCBS, num_MCBS, num_MCBS_tot, RX_eMBB = scenario_gen.sinr_gen (scn, sum(SCBS_per_MCBS), macro_cell_locations, np.asarray(locs_SCBS), usr_locs['user_locations'+str(i)], dsc, np)
+		sinr_sorted, locs_sc_ret, usr_lcs, idx_sc, idx_mc, sinr_pad, num_SCBS, num_MCBS, num_MCBS_tot, RX_eMBB, l_nl = scenario_gen.sinr_gen (scn, sum(SCBS_per_MCBS), macro_cell_locations, np.asarray(locs_SCBS), usr_locs['user_locations'+str(i)], dsc, np, int(vars(args)['interf']) )
 		sinr = dsc.reorganizer(sinr_sorted, idx_sc, idx_mc, num_SCBS, num_MCBS_tot, sinr_pad, np, scn); # We reorganize the SINR matrix for the optimization framework
 		RX = dsc.reorganizer(RX_eMBB, idx_sc, idx_mc, num_SCBS, num_MCBS_tot, float('nan'), np, scn); # We reorganize the RX Power matrix for the Baseline framework
 		# ================================
 		# Create Compressed Variable Files
 		
-		np.savez_compressed(os.getcwd()+'/Data/Temp/optim_var_'+ str(i) + str(vars(args)['iter']),sinr, usr_apps_assoc, usr_lcs, idx_sc, sinr_pad, num_SCBS, num_MCBS_tot, SC_wl_bh, SC_wrd_bh, MC_hops, SC_hops, BH_capacity_SC, RX, SCBS_per_MCBS, allow_pickle = True); # Save these variables to be utilized by the optimizer
-		np.savez_compressed(os.getcwd()+'/Data/Temp/hmap_' + str(i) + str(vars(args)['iter']), usr_lcs, locs_SCBS, macro_cell_locations, SCBS_per_MCBS, SCBS_MCBS_assoc) # Data necessary for heatmap is saved here
+		np.savez_compressed(os.getcwd()+'/Data/Temp/optim_var_'+ str(i) + str(vars(args)['iter']),sinr, usr_apps_assoc, usr_lcs, idx_sc, sinr_pad, num_SCBS, num_MCBS_tot, SC_wl_bh, SC_wrd_bh, MC_hops, SC_hops, BH_capacity_SC, RX, SCBS_per_MCBS, l_nl, allow_pickle = True); # Save these variables to be utilized by the optimizer
+		np.savez_compressed(os.getcwd()+'/Data/Temp/hmap_' + str(i) + str(vars(args)['iter']), usr_lcs, locs_SCBS, macro_cell_locations, SCBS_per_MCBS, SCBS_MCBS_assoc, l_nl, sinr) # Data necessary for heatmap is saved here
 		#np.savez_compressed('/home/akshayjain/Desktop/Simulation/optim_var_1',sinr_sorted, usr_apps_assoc, usr_lcs, idx_sc, sinr_pad, num_SCBS, num_MCBS, SC_wl_bh, SC_wrd_bh, MC_hops, SC_hops, BH_capacity_SC); # Save these variables to be utilized by the optimizer
 
 		# ===========================

optimizer_func.py
@@ -13,6 +13,7 @@ from argparse import ArgumentParser
 from rssi_assoc import baseline_assoc
 import time
 import csv
+import csvsaver
 
 # =======================
 # Optimizer Configuration
@@ -131,7 +132,8 @@ for N in range(0,num_iter):
 	 	SNR_eMBB[iter:iter + np.sum(user_AP_assoc[i,1]), :] = np.delete(np.outer(user_AP_assoc[i,1],SNR_iter[i,:]), np.where(user_AP_assoc[i,1] == 0), 0);# Application to Base Station SINR matrix 
 	 	iter = iter + np.sum(user_AP_assoc[i,1]); # Incrementing the iterator for the next user-application sets
 	
-	np.savetxt('log.csv',sinr_eMBB, delimiter=",")
+	#np.savetxt('SINR.csv',sinr_eMBB, delimiter=",")
+	csvsaver.csvsaver(np.where(sinr_eMBB == sinr_pad_val, 0, sinr_eMBB), [], "SINR.csv")
 	#print sinr_eMBB
 	#print sinr_applications[0]
 
@@ -202,7 +204,7 @@ for N in range(0,num_iter):
 
 		# ===> Establish the Objective Function
 
-		obj_func = quicksum(G_SC[i,j,k]*scn.BW_SC[k]*rate[i,j] for i in range(var_row_num) for j in range(num_scbs) for k in range(3)) + quicksum(G_MC[i,j,k]*scn.BW_MC[k]*rate[i,j] for i in range(var_row_num) for j in range(num_mcbs) for k in range(5)); 
+		obj_func = quicksum(G_SC[i,j,k]*scn.BW_SC[k]*rate[i,j] for i in range(var_row_num) for j in range(num_scbs) for k in range(3)) + quicksum(G_MC[i,j - num_scbs,k]*scn.BW_MC[k]*rate[i,j] for i in range(var_row_num) for j in range(num_scbs, num_scbs + num_mcbs) for k in range(5)); 
 		#obj_func = LinExpr(); # Initialize the objective function
 		#obj_func.addTerms(rate,X);
 		# for i in range(0,var_row_num):
@@ -222,16 +224,18 @@ for N in range(0,num_iter):
 
 		DC = m.addVars(var_row_num,1, name = "DC"); # Initializing the Constraint Variables
 		for i in range(0,var_row_num):
-			DC[i,0] = X.sum(i,'*'); # Constraint Expression
+			#DC[i,0] = X.sum(i,'*'); # Constraint Expression
+			DC[i,0] = quicksum(X[i,j] for j in range(var_col_num))
 		
-		MC = m.addVars(var_row_num,1, name = "MC"); # Initializing the Constraint Variables with MC 
+		MC = m.addVars(var_row_num,1, name = "K"); # Initializing the Constraint Variables with MC 
 		for i in range(0,var_row_num):
-			MC[i,0] = X.sum(i,np.arange(num_scbs,num_scbs+num_mcbs).tolist()); # Macro Cell Constraint Expression
+			#MC[i,0] = X.sum(i,np.arange(num_scbs,num_scbs+num_mcbs).tolist()); # Macro Cell Constraint Expression
+			MC[i,0] = quicksum(X[i,j] for j in range(num_scbs, num_mcbs+num_scbs)) # Macro Cell Constraint Expression
 
-		SC = m.addVars(var_row_num,1, name = "SC"); # Initializing the Constraint Variable with SC
+		SC = m.addVars(var_row_num,1, name = "J"); # Initializing the Constraint Variable with SC
 		for i in range(0,var_row_num):
-			SC[i,0] = X.sum(i,np.arange(0,num_scbs).tolist()); # Small Cell Constraint Expression
-
+			#SC[i,0] = X.sum(i,np.arange(0,num_scbs).tolist()); # Small Cell Constraint Expression
+			SC[i,0] = quicksum(X[i,j] for j in range(num_scbs)) # Small Cell Constraint Expression
 		# ======================================================= 
 		# Set up the Minimum Rate Constraint for the Applications
 
@@ -309,15 +313,17 @@ for N in range(0,num_iter):
 		# Unity Assignment Constraints
 
 		U_SC = m.addVars(var_row_num, int(num_scbs), name="USC");
+		
 		for i in range(var_row_num):
+			#U_SC[i,j] = LinExpr([1]*3,G_SC.select(i,j,'*'))
 			for j in range(num_scbs):
-				U_SC[i,j] = LinExpr([1]*3,G_SC.select(i,j,'*'))
+				U_SC[i,j] = quicksum(G_SC[i,j,k] for k in range(3))
 
 		U_MC = m.addVars(var_row_num, int(num_mcbs), name="UMC")
 		for i in range(var_row_num):
+			#U_MC[i,j] = LinExpr([1]*5,G_MC.select(i,j,'*'))
 			for j in range(num_mcbs):
-				U_MC[i,j] = LinExpr([1]*5,G_MC.select(i,j,'*'))
-
+				U_MC[i,j] = quicksum(G_MC[i,j,k] for k in range(5))
 
 
 		# ==========================
@@ -338,11 +344,11 @@ for N in range(0,num_iter):
 		m.addConstrs((G_SC[i,j,k] <= BW_SC[i,k] for i in range(var_row_num) for j in range(num_scbs) for k in range(3)), name = 'l1'); # Linearization constraint 1
 		m.addConstrs((G_SC[i,j,k] <= X[i,j] for i in range(var_row_num) for j in range(num_scbs) for k in range(3)), name = 'l2'); # Linearization constraint 2
 		m.addConstrs((G_SC[i,j,k] >= (BW_SC[i,k] + X[i,j] -1) for i in range(var_row_num) for j in range(num_scbs) for k in range(3)), name = 'l3'); # Linearization constraint 3
-		m.addConstrs((G_MC[i,j - num_scbs,k] <= BW_MC[i,k] for i in range(var_row_num) for j in range(num_scbs, num_scbs+num_mcbs) for k in range(5)), name = 'l1'); # Linearization constraint 4
-		m.addConstrs((G_MC[i,j - num_scbs,k] <= X[i,j] for i in range(var_row_num) for j in range(num_scbs, num_scbs+num_mcbs) for k in range(5)), name = 'l2'); # Linearization constraint 5
-		m.addConstrs((G_MC[i,j - num_scbs,k] >= (BW_MC[i,k] + X[i,j] -1) for i in range(var_row_num) for j in range(num_scbs, num_scbs+num_mcbs) for k in range(5)), name = 'l3'); # Linearization constraint 6
+		m.addConstrs((G_MC[i,j - num_scbs,k] <= BW_MC[i,k] for i in range(var_row_num) for j in range(num_scbs, num_scbs+num_mcbs) for k in range(5)), name = 'l4'); # Linearization constraint 4
+		m.addConstrs((G_MC[i,j - num_scbs,k] <= X[i,j] for i in range(var_row_num) for j in range(num_scbs, num_scbs+num_mcbs) for k in range(5)), name = 'l5'); # Linearization constraint 5
+		m.addConstrs((G_MC[i,j - num_scbs,k] >= (BW_MC[i,k] + X[i,j] -1) for i in range(var_row_num) for j in range(num_scbs, num_scbs+num_mcbs) for k in range(5)), name = 'l6'); # Linearization constraint 6
 		m.addConstrs((U_SC[i,j] <= 1 for i in range(var_row_num) for j in range(num_scbs)),name = 'U1')
-		m.addConstrs((U_MC[i,j] <= 1 for i in range(var_row_num) for j in range(num_mcbs)), name = 'U2')
+		m.addConstrs((U_MC[i,j] <= 1 for i in range(var_row_num) for j in range(num_mcbs)),name = 'U2')
 
 		if vars(args)['dual'] == 0:
 			print "==================="
@@ -369,8 +375,10 @@ for N in range(0,num_iter):
 			#m.addConstrs((U_SC[i,j] <= 1 for i in range(var_row_num) for j in range(num_scbs)))
 			#m.addConstrs((U_MC[i,j] == 1 for i in range(var_row_num) for j in range(num_mcbs)))
 			m.addConstrs((MC[i,0] == 1 for i in range(var_row_num)), name ='c'); # Adding the Dual Connectivity constraint 
+			#m.addConstrs((quicksum(X[i,j] for j in range(num_scbs, num_scbs+num_mcbs)) == 1 for i in range(var_row_num)), name ='c8'); # Adding the Dual Connectivity constraint 
 			m.addConstrs((SC[i,0] <= 1 for i in range(var_row_num)), name ='c5'); # Adding the Dual Connectivity constraint 
 			
+
 			#m.addConstrs((DC[i,0] >= 1 for i in range(var_row_num)), name ='c5'); # Adding the Dual Connectivity constraint 
 			if vars(args)['minRate'] == 1:
 				m.addConstrs((min_RATE[i,0] >= scn.eMBB_minrate for i in range(var_row_num)), name ='c1'); # Adding the minimum rate constraint
@@ -417,12 +425,15 @@ for N in range(0,num_iter):
 			M_plt = []
 			SCbw = []
 			MCbw = []
+			MC_sel = []
 
 			for v in m.getVars():
 				if "GSC" in v.varName:
 					G_plt.append(v.x)
 				if "GMC" in v.varName:
 					M_plt.append(v.x)
+				#if "" in v.varName:
+				#	print v.x
 			#	if "bwmc" in v.varName:
 			#		MCbw.append(v.x)
 			#	if "bwsc" in v.varName:
@@ -431,6 +442,7 @@ for N in range(0,num_iter):
 			# =============================
 			# Visualization and Computation			
 
+			
 			#fin_rate = np.zeros((var_row_num,1))
 			#for i in range(var_row_num):
 			#	for j in range(var_col_num):
@@ -462,19 +474,52 @@ for N in range(0,num_iter):
 
 			G_total_compute = np.concatenate((GSC_compute, GMC_compute), axis = 1) # Bandwidth Contribution matrix
 			new_rate = rate*G_total_compute; # New rate matrix
-			print np.sum(new_rate,axis  = 1)
-			print "============================"
-			print "BHCAP check"
-			bhval = 0; # Initialize the Utilized Bhaul capacity value
+			#print new_rate
+
+			# ==> Data rate each user gets from the SC and MC for MCSC - DC
+
+			Rate_data = np.empty((var_row_num,4))
+			G_sum = np.empty((num_scbs,1))
+			M_sum = np.empty((num_mcbs,1))
+			for i in range(var_row_num):
+				# print "=============="
+				# print ("For user #",i)
+				# #print new_rate[i,num_scbs + np.nonzero(new_rate[i,num_scbs:num_scbs+num_mcbs])]
+				# print("Small Cell Data Rate:", new_rate[i, np.nonzero(new_rate[i,:num_scbs])])
+				# print("Macro Cell Data Rate:", new_rate[i, num_scbs + np.nonzero(new_rate[i,num_scbs:num_scbs+num_mcbs])])
+				if new_rate[i, np.nonzero(new_rate[i,:num_scbs])].size == 0:
+					Rate_data[i,0] = 0
+				else:
+					Rate_data[i,0] = np.sum(new_rate[i, np.nonzero(new_rate[i,:num_scbs])])
+
+				if new_rate[i, num_scbs + np.nonzero(new_rate[i,num_scbs:num_scbs+num_mcbs])].size == 0:
+					Rate_data[i,1] = 0
+				else:
+					Rate_data[i,1] = np.sum(new_rate[i, num_scbs + np.nonzero(new_rate[i,num_scbs:num_scbs+num_mcbs])])
+				#print np.where(new_rate[i, num_scbs + np.nonzero(new_rate[i,num_scbs:num_scbs+num_mcbs])].size == 0 , "Size is 0", new_rate[i, num_scbs + np.nonzero(new_rate[i,num_scbs:num_scbs+num_mcbs])]) 
+
+			csvsaver.csvsaver(Rate_data,["SC Rate","MC Rate"], "OptimizedDataRate_user_MCSC.csv")
+			#print np.sum(new_rate, axis = 1)
+			print np.amin(np.sum(new_rate,axis  = 1))
+			print np.amax(np.sum(new_rate,axis  = 1))
+			print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+			print ("MINRATE Constraint satisfied?:", np.all(np.sum(new_rate,axis  = 1)>=scn.eMBB_minrate))
+			print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+			# #print "============================"
+			#print "BHCAP check"
+			#bhval = 0; # Initialize the Utilized Bhaul capacity value
 			
 			#		new_rate[i,j] = (np.sum(G_plt_idx[i,j,:]*np.asarray(scn.BW_SC),axis = 0) + np.sum(M_plt_idx[i,j,:]*np.asarray(scn.BW_MC),axis = 0))*rate[i,j]			
 	
-			G_sum = np.sum(G_plt_idx[:,:,0] + G_plt_idx[:,:,1] + G_plt_idx[:,:,2], axis = 1)
-			M_sum = np.sum(M_plt_idx[:,:,0] + M_plt_idx[:,:,1] + M_plt_idx[:,:,2] + M_plt_idx[:,:,3] + M_plt_idx[:,:,4], axis = 1)
+			G_sum[:,0] = np.sum(G_plt_idx[:,:,0] + G_plt_idx[:,:,1] + G_plt_idx[:,:,2], axis = 0)
+			M_sum[:,0] = np.sum(M_plt_idx[:,:,0] + M_plt_idx[:,:,1] + M_plt_idx[:,:,2] + M_plt_idx[:,:,3] + M_plt_idx[:,:,4], axis = 0)
 			#print G_sum.shape
 			#print M_sum.shape
 			#print ("SC:", G_sum)
 			#print ("MC:", M_sum)
+			csvsaver.csvsaver(G_sum,["Accepted Users per SC"], "GS.csv")
+			csvsaver.csvsaver(M_sum,["Accepted Users per MC"], "MC.csv")			
+
 			if N == (num_iter-1) and (vars(args)['dual'] == 1 or vars(args)['bhaul'] == 1 or vars(args)['minRate'] == 1 or vars(args)['latency'] == 1):
 				#plotter.optimizer_plotter(new_rate) # We get the plot for the rates with maximum number of users
 				with open("Rate" + str(vars(args)['iter']) + str(vars(args)['dual']) + str(vars(args)['bhaul']) + str(vars(args)['minRate']) + str(vars(args)['latency']) + ".csv", "w+") as my_csv:
@@ -491,13 +536,18 @@ for N in range(0,num_iter):
 
 			print "Saving Data"
 
-			Data['X_optimal_data' + str(N)] = np.asarray(X_optimal).reshape((var_row_num,var_col_num)); # Optimal Association Matrix
+			#Data['X_optimal_data' + str(N)] = np.asarray(X_optimal).reshape((var_row_num,var_col_num)); # Optimal Association Matrix
+			Data['X_optimal_data' + str(N)] = (G_total_compute>0)*1; # Optimal Association Matrix
 			Data['Net_Throughput' + str(N)] = m.objVal; # Network wide throughput
 			Data['Rates' + str(N)] = new_rate; # Data rate matrix  
 			Data['Status' + str(N)] = m.status; # Insert the status
 			Data['Apps'+str(N)] = var_row_num;
 			Data['APs'+str(N)] = var_col_num;
 
+			#print np.sum((G_total_compute>0)*1, axis = 1) 
+			#print np.sum((np.asarray(X_optimal).reshape((var_row_num,var_col_num))>0)*1, axis =1)
+			#print np.nonzero((np.asarray(X_optimal).reshape((var_row_num,var_col_num))>0)*1)
+			#print np.array_equal((G_total_compute>0)*1, (np.asarray(X_optimal).reshape((var_row_num,var_col_num))>0)*1)
 			# ========================
 			# Validity of the Solution
 

pathloss.py
@@ -14,92 +14,103 @@ def pathloss_CI(scn, dist, np, d3d, dsc, sc_flag):
    
     # =====================================================
     # We implement the NYU CI model for full distance range
+    
+    los_flag = 0 # Line of Sight and Non Line of Sight flag 
     if sc_flag == 0 or sc_flag == 1:
         FSPL = 20*np.log10((4*np.pi*np.where(sc_flag,scn.fc_sc,scn.fc_mc))/scn.c); # Calculate the Free Space Path loss
 
         # =====================================================================
         # We consider a LOS scenario if the los probability is greater than 50%
+        
+        #print ("The Threshold for SC LOS-NLOS is:", scn.tau_sc[tau_flag])
+        #print ("The Threshold for MC LOS-NLOS is:", scn.tau_mc[tau_flag])
 
-        if los_probability.los_prob_sc(np,dist) >= 0.5:
-    
+        if (np.random.rand(1) <= los_probability.los_prob(np,dist,sc_flag)  and sc_flag == 1) or (np.random.rand(1) <= los_probability.los_prob(np,dist,sc_flag) and sc_flag == 0):
+            los_flag = 1 # Return the los flag for the plots later
+            #print ("LOS with flag:",sc_flag)
             n = np.where(sc_flag, 2.1, 2.0); # For LOS scenarios UMa has PLE = 2.0 and UMi at 28 and 73GHz has PLE = 2.1
             SF_dev = np.where(sc_flag, 4.4, 2.4); # For LOS Scenarios UMa has a SF dev of 2.4 and UMi at 28 and 73 GHz has SF dev of 4.4 
             shadowing = np.random.normal(0,SF_dev);
             if dist < 1:
-                return (FSPL + shadowing)  # Below 1m it is PL is just FSPL in CI model
+                return (FSPL + shadowing), los_flag  # Below 1m it is PL is just FSPL in CI model
             else: 
                 PL_CI = FSPL + 10*n*np.log10(d3d) + shadowing; # CI model Pathloss 
-            return PL_CI
+            return PL_CI, los_flag
 
         else:
-            
-            n = np.where(sc_flag, 3.2, 2.9); # For LOS scenarios UMa has PLE = 2.0 and UMi at 28 and 73GHz has PLE = 2.1
-            SF_dev = np.where(sc_flag, 8.0, 5.7); # For LOS Scenarios UMa has a SF dev of 2.4 and UMi at 28 and 73 GHz has SF dev of 4.4 
+            los_flag = 0 # Return the los flag for the plots later (NLOS)
+            #print ("NLOS with flag:",sc_flag)
+            n = np.where(sc_flag, 3.2, 2.9); # For NLOS scenarios UMa has PLE = 2.0 and UMi at 28 and 73GHz has PLE = 2.1
+            SF_dev = np.where(sc_flag, 8.0, 5.7); # For NLOS Scenarios UMa has a SF dev of 2.4 and UMi at 28 and 73 GHz has SF dev of 4.4 
             shadowing = np.random.normal(0,SF_dev);
             if dist < 1:
-                return (FSPL + shadowing)  # Below 1m it is PL is just FSPL in CI model
+                return (FSPL + shadowing), los_flag  # Below 1m it is PL is just FSPL in CI model
             else: 
                 PL_CI = FSPL + 10*n*np.log10(d3d) + shadowing; # CI model Pathloss 
-            return PL_CI
+            return PL_CI, los_flag
             
     elif sc_flag == 2:
+        
+        #if los_probability.los_prob_sc(np,dist) >= 0.5: # Small Cells will always be in LOS for a MC
+        los_flag = 1
+        FSPL = 20*np.log10((4*np.pi*scn.fc_bh_sc)/scn.c); # Calculate the Free Space Path loss for Backhaul
+        n = 2.0; # We consider BH to be in LOS scenario with a pathloss exponent of 2.1
+        SF_dev = 4.2; # Standard deviation for Shadow Fading
+        shadowing = np.random.normal(0, SF_dev);
+        if dist < 1:
+            return (FSPL+shadowing), los_flag; # Unusual between SC and MC but can happen
+        else: 
+            PL_SC_MC_CI = FSPL + 10*n*np.log10(d3d) + shadowing; # CI model Pathloss between SC and MC
+        return PL_SC_MC_CI, los_flag
+        # else:
+        #     los_flag = 1
+        #     FSPL = 20*np.log10((4*np.pi*scn.fc_bh_sc)/scn.c); # Calculate the Free Space Path loss for Backhaul
+        #     n = 3.5; # We consider BH to be in LOS scenario with a pathloss exponent of 2.1
+        #     SF_dev = 7.9; # Standard deviation for Shadow Fading
+        #     shadowing = np.random.normal(0, SF_dev);
+        #     if dist < 1:
+        #         return (FSPL+shadowing); # Unusual between SC and MC but can happen
+        #     else: 
+        #         PL_SC_MC_CI = FSPL + 10*n*np.log10(d3d) + shadowing; # CI model Pathloss between SC and MC
+        #     return PL_SC_MC_CI, los_flag
 
-        if los_probability.los_prob_sc(np,dist) >= 0.5:
-            FSPL = 20*np.log10((4*np.pi*scn.fc_bh_sc)/scn.c); # Calculate the Free Space Path loss for Backhaul
-            n = 2.0; # We consider BH to be in LOS scenario with a pathloss exponent of 2.1
-            SF_dev = 4.2; # Standard deviation for Shadow Fading
-            shadowing = np.random.normal(0, SF_dev);
-            if dist < 1:
-                return (FSPL+shadowing); # Unusual between SC and MC but can happen
-            else: 
-                PL_SC_MC_CI = FSPL + 10*n*np.log10(d3d) + shadowing; # CI model Pathloss between SC and MC
-            return PL_SC_MC_CI
-        else:
-            FSPL = 20*np.log10((4*np.pi*scn.fc_bh_sc)/scn.c); # Calculate the Free Space Path loss for Backhaul
-            n = 3.5; # We consider BH to be in LOS scenario with a pathloss exponent of 2.1
-            SF_dev = 7.9; # Standard deviation for Shadow Fading
-            shadowing = np.random.normal(0, SF_dev);
-            if dist < 1:
-                return (FSPL+shadowing); # Unusual between SC and MC but can happen
-            else: 
-                PL_SC_MC_CI = FSPL + 10*n*np.log10(d3d) + shadowing; # CI model Pathloss between SC and MC
-            return PL_SC_MC_CI
+
 
 
 # ============================================
 # 3GPP Small Cell Pathloss 
 # ============================================
 
-def pathloss_SC_3GPP(scn, dist, np, d3d, dsc):
+# def pathloss_SC_3GPP(scn, dist, np, d3d, dsc):
    
-    # =====================================================================
-    # We consider a LOS scenario if the los probability is greater than 50%
+#     # =====================================================================
+#     # We consider a LOS scenario if the los probability is greater than 50%
     
-    pathloss_sc = 0; # Declaring the pathloss variable
-    #bs_ht = 10; # Small cell base station height is 10m
-    if los_probability.los_prob_sc(np,dist) >= 0.5:
-        bp_dst = dsc.breakpt_dist(scn, dist, 1, np); # Breakpoint distance 
-        if dist>=10 and dist<bp_dst:
-            pathloss_sc = 32.4 + 21*np.log10(d3d)+20*np.log10(scn.fc_sc); 
-        elif dist >= bp_dst and dist <= 5000:
-            pathloss_sc = 32.4 + 40*np.log10(d3d) + 20*np.log10(scn.fc_sc) -9.5*np.log10((bp_dst)**2 + (scn.bs_ht_sc - scn.usr_ht)**2)
-        shadowing = np.random.normal(0,4); # We introduce shadowing
-        return (pathloss_sc+shadowing) # We return the total large scale fading
+#     pathloss_sc = 0; # Declaring the pathloss variable
+#     #bs_ht = 10; # Small cell base station height is 10m
+#     if los_probability.los_prob_sc(np,dist) >= 0.5:
+#         bp_dst = dsc.breakpt_dist(scn, dist, 1, np); # Breakpoint distance 
+#         if dist>=10 and dist<bp_dst:
+#             pathloss_sc = 32.4 + 21*np.log10(d3d)+20*np.log10(scn.fc_sc); 
+#         elif dist >= bp_dst and dist <= 5000:
+#             pathloss_sc = 32.4 + 40*np.log10(d3d) + 20*np.log10(scn.fc_sc) -9.5*np.log10((bp_dst)**2 + (scn.bs_ht_sc - scn.usr_ht)**2)
+#         shadowing = np.random.normal(0,4); # We introduce shadowing
+#         return (pathloss_sc+shadowing) # We return the total large scale fading
 
-    # ===================================================
-    # We now consider the N-LOS scenario
+#     # ===================================================
+#     # We now consider the N-LOS scenario
     
-    else:
-        bp_dst = dsc.breakpt_dist(scn, dist, 1, np); # Breakpoint distance 
-        if dist>=10 and dist<=5000:
-            if dist>=10 and dist<bp_dst:
-                los_sc = 32.4 + 21*np.log10(d3d)+20*np.log10(scn.fc_sc); 
-            elif dist >= bp_dst and dist <= 5000:
-                los_sc = 32.4 + 40*np.log10(d3d) + 20*np.log10(scn.fc_sc) -9.5*np.log10((bp_dst)**2 + (scn.bs_ht_sc - scn.usr_ht)**2);
-            nlos_sc = 35.3*np.log10(d3d)+22.4+21.3*np.log10(scn.fc_sc)-0.3*(scn.usr_ht-1.5);
-            pathloss_sc = np.maximum(los_sc,nlos_sc)      
-            shadowing = np.random.normal(0,7.82); #Shadowing in NLOS scenarios
-    return (pathloss_sc+shadowing)
+#     else:
+#         bp_dst = dsc.breakpt_dist(scn, dist, 1, np); # Breakpoint distance 
+#         if dist>=10 and dist<=5000:
+#             if dist>=10 and dist<bp_dst:
+#                 los_sc = 32.4 + 21*np.log10(d3d)+20*np.log10(scn.fc_sc); 
+#             elif dist >= bp_dst and dist <= 5000:
+#                 los_sc = 32.4 + 40*np.log10(d3d) + 20*np.log10(scn.fc_sc) -9.5*np.log10((bp_dst)**2 + (scn.bs_ht_sc - scn.usr_ht)**2);
+#             nlos_sc = 35.3*np.log10(d3d)+22.4+21.3*np.log10(scn.fc_sc)-0.3*(scn.usr_ht-1.5);
+#             pathloss_sc = np.maximum(los_sc,nlos_sc)      
+#             shadowing = np.random.normal(0,7.82); #Shadowing in NLOS scenarios
+#     return (pathloss_sc+shadowing)
 
 
 # =====================================================
@@ -107,33 +118,33 @@ def pathloss_SC_3GPP(scn, dist, np, d3d, dsc):
 # =====================================================
 
 
-def pathloss_MC_3GPP(scn, dist, np, d3d, dsc):
+# def pathloss_MC_3GPP(scn, dist, np, d3d, dsc):
 
-    # ==================================================
-    # We consider a LOS scenario if the los probability is greater than 50%
+#     # ==================================================
+#     # We consider a LOS scenario if the los probability is greater than 50%
     
-    #bs_ht = 25; # Macro cell base station height is 25m
-    if los_probability.los_prob_mc(np,dist,dsc.los_prob_var_gen(scn.usr_ht)) >= 0.5:
-        bp_dst = dsc.breakpt_dist(scn, dist, 0, np); # Breakpoint distance 
-        if dist>=10 and dist<bp_dst:
-            pathloss_mc = 32.4 + 20*np.log10(d3d)+20*np.log10(scn.fc); 
-        elif dist >= bp_dst and dist <= 5000:
-            pathloss_mc = 32.4 + 40*np.log10(d3d) + 20*np.log10(scn.fc) -10*np.log10((bp_dst)**2 + (scn.bs_ht - scn.usr_ht)**2)
-        shadowing = np.random.normal(0,4); # We introduce shadowing
-        return (pathoss_mc+shadowing) # We return the total large scale fading
+#     #bs_ht = 25; # Macro cell base station height is 25m
+#     if los_probability.los_prob_mc(np,dist,dsc.los_prob_var_gen(scn.usr_ht)) >= 0.5:
+#         bp_dst = dsc.breakpt_dist(scn, dist, 0, np); # Breakpoint distance 
+#         if dist>=10 and dist<bp_dst:
+#             pathloss_mc = 32.4 + 20*np.log10(d3d)+20*np.log10(scn.fc); 
+#         elif dist >= bp_dst and dist <= 5000:
+#             pathloss_mc = 32.4 + 40*np.log10(d3d) + 20*np.log10(scn.fc) -10*np.log10((bp_dst)**2 + (scn.bs_ht - scn.usr_ht)**2)
+#         shadowing = np.random.normal(0,4); # We introduce shadowing
+#         return (pathoss_mc+shadowing) # We return the total large scale fading
 
-    # ===================================================
-    # We now consider the N-LOS scenario
+#     # ===================================================
+#     # We now consider the N-LOS scenario
     
-    else:
-        bp_dst = dsc.breakpt_dist(scn, dist, 0, np); # Breakpoint distance 
-        if dist>=10 and dist<=5000:
-            if dist>=10 and dist<bp_dst:
-                los_mc = 32.4 + 20*np.log10(d3d)+20*np.log10(scn.fc); 
-            elif dist >= bp_dst and dist <= 5000:
-                los_mc = 32.4 + 40*np.log10(d3d) + 20*np.log10(scn.fc) -10*np.log10((bp_dst)**2 + (scn.bs_ht - scn.usr_ht)**2);
-            nlos_mc = 39.08*np.log10(d3d)+13.54+20*np.log10(scn.fc)-0.6*(scn.usr_ht-1.5);
-            pathloss_mc = np.maximum(los_sc,nlos_sc)      
-            shadowing = np.random.normal(0,7.8); #Shadowing in NLOS scenarios
-    return (pathloss_mc+shadowing)
+#     else:
+#         bp_dst = dsc.breakpt_dist(scn, dist, 0, np); # Breakpoint distance 
+#         if dist>=10 and dist<=5000:
+#             if dist>=10 and dist<bp_dst:
+#                 los_mc = 32.4 + 20*np.log10(d3d)+20*np.log10(scn.fc); 
+#             elif dist >= bp_dst and dist <= 5000:
+#                 los_mc = 32.4 + 40*np.log10(d3d) + 20*np.log10(scn.fc) -10*np.log10((bp_dst)**2 + (scn.bs_ht - scn.usr_ht)**2);
+#             nlos_mc = 39.08*np.log10(d3d)+13.54+20*np.log10(scn.fc)-0.6*(scn.usr_ht-1.5);
+#             pathloss_mc = np.maximum(los_sc,nlos_sc)      
+#             shadowing = np.random.normal(0,7.8); #Shadowing in NLOS scenarios
+#     return (pathloss_mc+shadowing)
         

plotgen.py
@@ -28,17 +28,28 @@ MCMC_iter = scn.MCMC_iter; # Number of Iterations to be analyzed
 simdata_path = os.getcwd() + '/Data/Process/'
 constraint_fp = {'Baseline':'0000', 'DC':'1000', 'DC_MRT':'1100','DC_BHCAP':'1010', 'DC_BHCAP_LAT':'1011', 'DC_LAT':'1001', 'DC_MRT_LAT':'1101', 'SA_MRT':'0100','SA_BHCAP':'0010','SA_BHCAP_LAT':'0011','SA_LAT':'0001','SA_MRT_LAT':'0101'}
 num_iter = ((scn.num_users_max - scn.num_users_min)/scn.user_steps_siml); 
+
 rate_matrix_DC = []
 rate_matrix_DC_MRT = []
 rate_matrix_DC_MRT_LAT = []
 rate_matrix_DC_BHCAP_LAT = []
 rate_matrix_DC_BHCAP = []
+rate_matrix_DC_LAT = []
 rate_matrix_SA_MRT = []
 rate_matrix_SA_LAT = []
 rate_matrix_SA_BHCAP = []
 rate_matrix_SA_BHCAP_LAT = []
 rate_matrix_SA_MRT_LAT = []
 rate_matrix_SA = []
+
+bhutil_val_DC = []
+bhutil_val_DC_BHCAP = []
+bhutil_val_DC_BHCAP_LAT = []
+latprov_DC_MRT_LAT = []
+latprov_DC = []
+latprov_DC_BHCAP_LAT = []
+latprov_DC_LAT = []
+avail_bh = []
 
 Net_Throughput = np.empty((MCMC_iter, num_iter));
 Net_Throughput_DC = copy.copy(Net_Throughput);
@@ -102,6 +113,8 @@ AU_DR_SA_MRT_LAT = np.zeros((MCMC_iter, num_iter))
 AU_DR_DC_MRT_LAT = np.zeros((MCMC_iter, num_iter))
 
 avg_idx = []; # This is for calculating the average application throughput 
+
+
 
 
 # ========================
@@ -191,22 +204,80 @@ for i in range(0,MCMC_iter):
 	Data_SA_MRT_LAT = Dat_SA_MRT_LAT['arr_0'];
 	Data_DC_MRT_LAT = Dat_DC_MRT_LAT['arr_0'];
 
-	if i == 1:
-		temp = Data_DC.item()['Rates'+str(1)]
-		rate_matrix_DC = np.sum(temp, axis = 1).tolist()
-		temp1 = Data.item()['Rates'+str(1)]
-		rate_matrix_SA = np.sum(temp1, axis = 1).tolist()
-		rate_matrix_DC_MRT = np.sum(Data_DC_MRT.item()['Rates'+str(1)], axis=1).tolist()
-		rate_matrix_DC_BHCAP_LAT = np.sum(Data_DC_BHCAP_LAT.item()['Rates'+str(1)], axis=1).tolist()
-		rate_matrix_DC_LAT = np.sum(Data_DC_LAT.item()['Rates'+str(1)], axis=1).tolist()
-		rate_matrix_SA_MRT = np.sum(Data_SA_MRT.item()['Rates'+str(1)], axis=1).tolist()
-		rate_matrix_SA_LAT = np.sum(Data_SA_MRT.item()['Rates'+str(1)], axis=1).tolist()
-		rate_matrix_SA_BHCAP = np.sum(Data_SA_BHCAP.item()['Rates'+str(1)], axis=1).tolist()
-		rate_matrix_SA_BHCAP_LAT = np.sum(Data_SA_BHCAP_LAT.item()['Rates'+str(1)], axis=1).tolist()
-		rate_matrix_SA_MRT_LAT = np.sum(Data_SA_MRT_LAT.item()['Rates'+str(1)], axis=1).tolist()
-		rate_matrix_DC_MRT_LAT = np.sum(Data_DC_MRT_LAT.item()['Rates'+str(1)], axis=1).tolist()
-		rate_matrix_DC_BHCAP = np.sum(Data_DC_BHCAP.item()['Rates'+str(1)], axis=1).tolist()
 
+	if i == 1:
+		if Data_DC.item()['Status'+str(5)] == 2:
+			bhutil_val_DC = (Data_DC.item()['BHUTIL'+str(5)].tolist())
+			latprov_DC = (Data_DC.item()['LatenOff'+str(5)].tolist())
+		else:
+			bhutil_val_DC = (np.zeros(Data_DC.item()['APs'+str(5)]).tolist())
+			latprov_DC = (np.zeros(Data_DC.item()['APs'+str(5)]).tolist())
+		
+		if Data_DC_LAT.item()['Status'+str(5)] == 2:
+			latprov_DC_LAT = (Data_DC_LAT.item()['LatenOff'+str(5)])
+			print latprov_DC_LAT
+		else:
+			latprov_DC_LAT = (np.zeros(Data_DC_LAT.item()['APs'+str(5)]).tolist())
+
+		if Data_DC_BHCAP_LAT.item()['Status'+str(5)] == 2: 
+			bhutil_val_DC_BHCAP_LAT = (Data_DC_BHCAP_LAT.item()['BHUTIL'+str(5)].tolist())
+			latprov_DC_BHCAP_LAT = (Data_DC_BHCAP_LAT.item()['LatenOff'+str(5)].tolist())
+		else: 
+			bhutil_val_DC_BHCAP_LAT = (np.zeros(Data_DC_BHCAP_LAT.item()['APs'+str(5)]).tolist())
+			latprov_DC_BHCAP_LAT = (np.zeros(Data_DC_BHCAP_LAT.item()['APs'+str(5)]).tolist())	
+		
+		if Data_DC_BHCAP.item()['Status'+str(5)] == 2: 
+			bhutil_val_DC_BHCAP = (Data_DC_BHCAP.item()['BHUTIL'+str(5)].tolist())
+		else:
+			bhutil_val_DC_BHCAP = (np.zeros(Data_DC_BHCAP.item()['APs'+str(5)]).tolist())
+
+		if Data_DC_MRT_LAT.item()['Status'+str(5)] == 2: 
+			latprov_DC_MRT_LAT = (Data_DC_MRT_LAT.item()['LatenOff'+str(5)].tolist())
+		else:
+			latprov_DC_MRT_LAT = (np.zeros(Data_DC_MRT_LAT.item()['APs'+str(5)]).tolist())	
+
+		#print Data_DC_LAT.item()['X_optimal_data'+str(1)]
+		avail_bh = Data_DC.item()['AvailBHUtil_SC'+str(5)]
+		#print avail_bh
+
+	if i == 1:
+		if Data_DC.item()['Status'+str(1)] == 2:
+			#temp = Data_DC.item()['Rates'+str(1)]
+			rate_matrix_DC = np.sum(Data_DC.item()['Rates'+str(1)], axis = 1).tolist()
+		if Data.item()['Status'+str(1)] == 2:
+			#temp1 = Data.item()['Rates'+str(1)]
+			rate_matrix_SA = np.sum(Data.item()['Rates'+str(1)], axis = 1).tolist()
+		
+		if Data_DC_MRT.item()['Status'+str(1)] == 2:
+			rate_matrix_DC_MRT = np.sum(Data_DC_MRT.item()['Rates'+str(1)], axis=1).tolist()
+	
+		if Data_DC_BHCAP_LAT.item()['Status'+str(1)] == 2:
+			rate_matrix_DC_BHCAP_LAT = np.sum(Data_DC_BHCAP_LAT.item()['Rates'+str(1)], axis=1).tolist()
+	
+		if Data_DC_LAT.item()['Status'+str(1)] == 2:
+			rate_matrix_DC_LAT = np.sum(Data_DC_LAT.item()['Rates'+str(1)], axis=1).tolist()
+		
+		if Data_DC_MRT_LAT.item()['Status'+str(1)] == 2:
+			rate_matrix_DC_MRT_LAT = np.sum(Data_DC_MRT_LAT.item()['Rates'+str(1)], axis=1).tolist()
+
+		if Data_DC_BHCAP.item()['Status'+str(1)] == 2:
+			rate_matrix_DC_BHCAP = np.sum(Data_DC_BHCAP.item()['Rates'+str(1)], axis=1).tolist()
+
+		if Data_SA_MRT.item()['Status'+str(1)] == 2:
+			rate_matrix_SA_MRT = np.sum(Data_SA_MRT.item()['Rates'+str(1)], axis=1).tolist()
+	
+		if Data_SA_LAT.item()['Status'+str(1)] == 2:
+			rate_matrix_SA_LAT = np.sum(Data_SA_LAT.item()['Rates'+str(1)], axis=1).tolist()
+	
+		if Data_SA_BHCAP.item()['Status'+str(1)] == 2:
+			rate_matrix_SA_BHCAP = np.sum(Data_SA_BHCAP.item()['Rates'+str(1)], axis=1).tolist()
+		
+		if Data_SA_BHCAP_LAT.item()['Status'+str(1)] == 2:			
+			rate_matrix_SA_BHCAP_LAT = np.sum(Data_SA_BHCAP_LAT.item()['Rates'+str(1)], axis=1).tolist()
+		
+		if Data_SA_MRT_LAT.item()['Status'+str(1)] == 2:			
+			rate_matrix_SA_MRT_LAT = np.sum(Data_SA_MRT_LAT.item()['Rates'+str(1)], axis=1).tolist()
+			
 
 	for k in range(0,num_iter):
 		if Data.item()['Status' + str(k)] == 2:
@@ -705,14 +776,16 @@ plt.savefig('Boxplot', dpi=1200, facecolor='w', edgecolor='w',
 # Heatmap Plotter
 
 plt.close("all")
-hmap_data = np.load(os.getcwd() +'/Data/Temp/hmap_119.npz', allow_pickle='True')
+hmap_data = np.load(os.getcwd() +'/Data/Temp/hmap_10.npz', allow_pickle='True')
 usr_locs = hmap_data['arr_0']
 mc_locs = hmap_data['arr_2']
 sc_locs = hmap_data['arr_1']
 
 
 
-#plotter.hmap_creator(usr_locs, mc_locs, sc_locs, rate_matrix_DC_BHCAP_LAT, np, scn)
-plotter.hist_plotter(rate_matrix_DC, rate_matrix_SA, rate_matrix_DC_BHCAP, rate_matrix_SA_BHCAP, rate_matrix_SA_LAT, rate_matrix_SA_MRT, rate_matrix_DC_MRT, rate_matrix_DC_LAT, rate_matrix_SA_MRT_LAT, rate_matrix_DC_MRT_LAT, rate_matrix_SA_BHCAP_LAT, rate_matrix_DC_BHCAP_LAT, np, scn)
-#plotter.scatter_plotter(rate_matrix_DC, rate_matrix_SA,np,scn)
+#plotter.hmap_creator(usr_locs, mc_locs, sc_locs, rate_matrix_DC, np, scn)
+#plotter.hist_plotter(rate_matrix_DC, rate_matrix_SA, rate_matrix_DC_BHCAP, rate_matrix_SA_BHCAP, rate_matrix_SA_LAT, rate_matrix_SA_MRT, rate_matrix_DC_MRT, rate_matrix_DC_LAT, rate_matrix_SA_MRT_LAT, rate_matrix_DC_MRT_LAT, rate_matrix_SA_BHCAP_LAT, rate_matrix_DC_BHCAP_LAT, np, scn)
+#plotter.scatter_plotter(rate_matrix_DC, rate_matrix_DC_BHCAP_LAT,np,scn)
 #plotter.accepted_user_plotter(AU_Base_DR_avg,AU_DR_avg,AU_DR_DC_avg,AU_DR_DC_MRT_avg,AU_DR_DC_BHCAP_avg,AU_DR_DC_LAT_avg,AU_DR_DC_BHCAP_LAT_avg,AU_DR_SA_MRT_avg,AU_DR_SA_LAT_avg,AU_DR_SA_BHCAP_avg,AU_DR_SA_BHCAP_LAT_avg,AU_DR_SA_MRT_LAT_avg,AU_DR_DC_MRT_LAT_avg,np,scn)
+#plotter.bhutil_latprov_plotter(bhutil_val_DC, bhutil_val_DC_BHCAP, bhutil_val_DC_BHCAP_LAT, avail_bh, latprov_DC, latprov_DC_LAT, latprov_DC_MRT_LAT, latprov_DC_BHCAP_LAT, np, scn)
+#plotter.infeasible_iter_counter(iters_infeas, iters_infeas_DC, iters_infeas_DC_MRT, iters_infeas_DC_LAT, iters_infeas_DC_BHCAP, iters_infeas_DC_BHCAP_LAT, iters_infeas_SA_LAT, iters_infeas_SA_BHCAP, iters_infeas_SA_BHCAP_LAT, iters_infeas_SA_MRT_LAT, iters_infeas_SA_MRT ,np,scn)

plotter.py
@@ -14,7 +14,7 @@ from scipy.stats import norm
 import seaborn as sns
 import os
 from mpl_toolkits.mplot3d import Axes3D
-
+from matplotlib import cm
 
 # =======
 # Plotter
@@ -74,49 +74,51 @@ def optimizer_plotter(data_plt):
 def hmap_creator(usr_lcs, mc_lcs, sc_lcs, rate_mat, np, scn):
 	f,ax = plt.subplots()
 	ax = plt.gca()
-	
-	s1, = ax.plot(usr_lcs[:,0], usr_lcs[:,1], "r+") # Plot the User locations
-	s2, = ax.plot(mc_lcs[:,0],mc_lcs[:,1],"b*") # Plot the macro cell locations
-	s3, = ax.plot(sc_lcs[:,0],sc_lcs[:,1],"g*") # Plot the small cell locations
+	print usr_lcs.shape[0]
+	print rate_mat 
+	#s1, = ax.plot(usr_lcs[:,0], usr_lcs[:,1], "r*", markersize=12) # Plot the User locations
+	s2, = ax.plot(mc_lcs[:,0],mc_lcs[:,1],"k^", markersize=12) # Plot the macro cell locations
+	s3, = ax.plot(sc_lcs[:,0],sc_lcs[:,1],"g^", markersize=8) # Plot the small cell locations
 	
 	# Create the color range
 	range_rate = np.arange(np.amin(rate_mat),np.amax(rate_mat),(np.amax(rate_mat)-np.amin(rate_mat))/7) # These are rate bands for the circular colors
-	color_range = ["ko","go","bo","ro","mo","co","yo"] # These are the color codes
+	color_range = ['#ffa07a','m','b','r','#daa520','#b22222','#8b0000'] # These are the color codes
 	circle_size = np.arange(6,13,1) # Circle size range
 	# color_sel = [] # Empty list to hold the color code and circle sizes
 	# # # Establish rate based circles on the plot 
 	for i in range(usr_lcs.shape[0]):
 		if rate_mat[i] >= range_rate[0] and rate_mat[i] < range_rate[1]:
 			#ax.plot(usr_lcs[i,0],usr_lcs[i,1], color_range[0], markersize=circle_size[0], fillstyle='none')
-			s4, = ax.plot(usr_lcs[i,0],usr_lcs[i,1], color_range[0], markersize=circle_size[0])
+			s4, = ax.plot(usr_lcs[i,0],usr_lcs[i,1], marker = 'o', markeredgecolor = color_range[0], markerfacecolor = color_range[0], markersize=circle_size[0])
 						
 		elif rate_mat[i] >= range_rate[1] and rate_mat[i] < range_rate[2]:
 			#ax.plot(usr_lcs[i,0],usr_lcs[i,1], color_range[1], markersize=circle_size[1], fillstyle='none')
-			s5, = ax.plot(usr_lcs[i,0],usr_lcs[i,1], color_range[1], markersize=circle_size[1])
+			s5, = ax.plot(usr_lcs[i,0],usr_lcs[i,1], marker = 'o', markeredgecolor = color_range[1], markerfacecolor = color_range[1], markersize=circle_size[1])
 			
 		elif rate_mat[i] >= range_rate[2] and rate_mat[i] < range_rate[3]:
 			#ax.plot(usr_lcs[i,0],usr_lcs[i,1], color_range[2], markersize=circle_size[2], fillstyle='none')
-			s6, = ax.plot(usr_lcs[i,0],usr_lcs[i,1], color_range[2], markersize=circle_size[2])
+			s6, = ax.plot(usr_lcs[i,0],usr_lcs[i,1], marker = 'o', markeredgecolor = color_range[2], markerfacecolor = color_range[2], markersize=circle_size[2])
 			
 		elif rate_mat[i] >= range_rate[3] and rate_mat[i] < range_rate[4]:
 			#ax.plot(usr_lcs[i,0],usr_lcs[i,1], color_range[3], markersize=circle_size[3], fillstyle='none')
-			s7, = ax.plot(usr_lcs[i,0],usr_lcs[i,1], color_range[3], markersize=circle_size[3])
+			s7, = ax.plot(usr_lcs[i,0],usr_lcs[i,1], marker = 'o',markeredgecolor = color_range[3], markerfacecolor = color_range[3], markersize=circle_size[3])
 			
 		elif rate_mat[i] >= range_rate[4] and rate_mat[i] < range_rate[5]:
 			#ax.plot(usr_lcs[i,0],usr_lcs[i,1], color_range[4], markersize=circle_size[4], fillstyle='none')
-			s8, = ax.plot(usr_lcs[i,0],usr_lcs[i,1], color_range[4], markersize=circle_size[4])
+			s8, = ax.plot(usr_lcs[i,0],usr_lcs[i,1], marker = 'o', markeredgecolor = color_range[4], markerfacecolor = color_range[4], markersize=circle_size[4])
 			
 		elif rate_mat[i] >= range_rate[5] and rate_mat[i] < range_rate[6]:
 			#ax.plot(usr_lcs[i,0],usr_lcs[i,1], color_range[5], markersize=circle_size[5], fillstyle='none')
-			s9, = ax.plot(usr_lcs[i,0],usr_lcs[i,1], color_range[5], markersize=circle_size[5])
+			s9, = ax.plot(usr_lcs[i,0],usr_lcs[i,1], marker = 'o', markeredgecolor = color_range[5], markerfacecolor = color_range[5], markersize=circle_size[5])
 			
 		else:
 			#ax.plot(usr_lcs[i,0],usr_lcs[i,1], color_range[6], markersize=circle_size[6], fillstyle='none')
-			s10, = ax.plot(usr_lcs[i,0],usr_lcs[i,1], color_range[6], markersize=circle_size[6])
+			s10, = ax.plot(usr_lcs[i,0],usr_lcs[i,1], marker = 'o', markeredgecolor = color_range[6], markerfacecolor = color_range[6], markersize=circle_size[6])
 
-	legend_cols = [s1, s2, s3, s4, s5, s6, s7, s8, s9, s10]
-	plt.legend(legend_cols,["Users", "Macro Cells", "Small Cells", str(format(range_rate[0],'0.6e'))+'--'+str(format(range_rate[1],'0.6e')),str(format(range_rate[1],'.6e'))+'--'+str(format(range_rate[2],'.6e')),str(format(range_rate[2],'0.6e'))+'--'+str(format(range_rate[3],'0.6e')),str(format(range_rate[3],'0.6e'))+'--'+str(format(range_rate[4],'0.6e')),str(format(range_rate[4],'0.6e'))+'--'+str(format(range_rate[5],'0.6e')),str(format(range_rate[5],'0.6e'))+'--'+str(format(range_rate[6],'0.6e'))],loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol = 5)	
-	plt.title("Heatmap of Individual User Data Rates (DC+BHCAP+LAT Scenario)")
+	#legend_cols = [ s2, s3, s4, s5, s6, s7, s8, s9, s10]
+	legend_cols = [ s2, s3, s4, s5, s10]
+	plt.legend(legend_cols,["Macro Cells", "Small Cells", str(format(range_rate[0],'0.6e'))+'--'+str(format(range_rate[1],'0.6e')),str(format(range_rate[1],'.6e'))+'--'+str(format(range_rate[2],'.6e')),str(format(range_rate[2],'0.6e'))+'--'+str(format(range_rate[3],'0.6e')),str(format(range_rate[3],'0.6e'))+'--'+str(format(range_rate[4],'0.6e')),str(format(range_rate[4],'0.6e'))+'--'+str(format(range_rate[5],'0.6e')),str(format(range_rate[5],'0.6e'))+'--'+str(format(range_rate[6],'0.6e'))],loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol = 4)	
+	plt.title("Heatmap of Individual User Data Rates (DC Scenario)")
 	plt.show()
 
 # =============================
@@ -152,19 +154,19 @@ def hist_plotter(rate_matrix_DC, rate_matrix_SA, rate_matrix_DC_BHCAP, rate_matr
 	
 
 	plt.plot(bincenters1,y1,'r-o',fillstyle='none')
-	#plt.plot(bincenters2,y2,'b-o',fillstyle='none')
+	plt.plot(bincenters2,y2,'b-o',fillstyle='none')
 	#plt.plot(bincenters3,y3,'b-o',fillstyle='none')
-	plt.plot(bincenters5,y5,'b-*',fillstyle='none')
+	#plt.plot(bincenters5,y5,'b-*',fillstyle='none')
 	plt.plot(bincenters1[y1.tolist().index(np.amax(y1))],np.amax(y1),'ko')
 	plt.plot(np.ones((np.arange(0,np.amax(y1)).shape[0]+1,1))*bincenters1[y1.tolist().index(np.amax(y1))],np.arange(0,np.amax(y1)+1),'k--')
-	#plt.plot(bincenters2[y2.tolist().index(np.amax(y2))],np.amax(y2),'go')
-	#plt.plot(np.ones((np.arange(0,np.amax(y2)).shape[0]+1,1))*bincenters2[y2.tolist().index(np.amax(y2))],np.arange(0,np.amax(y2)+1),'g--')
+	plt.plot(bincenters2[y2.tolist().index(np.amax(y2))],np.amax(y2),'go')
+	plt.plot(np.ones((np.arange(0,np.amax(y2)).shape[0]+1,1))*bincenters2[y2.tolist().index(np.amax(y2))],np.arange(0,np.amax(y2)+1),'g--')
 	#plt.plot(bincenters3[y3.tolist().index(np.amax(y3))],np.amax(y3),'go')
 	#plt.plot(np.ones((np.arange(0,np.amax(y3)).shape[0],1))*bincenters3[y3.tolist().index(np.amax(y3))],np.arange(0,np.amax(y3)),'g--')
 	#plt.plot(bincenters7[y7.tolist().index(np.amax(y7))],np.amax(y7),'go')
 	#plt.plot(np.ones((np.arange(0,np.amax(y7)).shape[0],1))*bincenters7[y7.tolist().index(np.amax(y7))],np.arange(0,np.amax(y7)),'g--')
-	plt.plot(bincenters5[y5.tolist().index(np.amax(y5))],np.amax(y5),'go')
-	plt.plot(np.ones((np.arange(0,np.amax(y5)).shape[0],1))*bincenters5[y5.tolist().index(np.amax(y5))],np.arange(0,np.amax(y5)),'g--')
+	#plt.plot(bincenters5[y5.tolist().index(np.amax(y5))],np.amax(y5),'go')
+	#plt.plot(np.ones((np.arange(0,np.amax(y5)).shape[0],1))*bincenters5[y5.tolist().index(np.amax(y5))],np.arange(0,np.amax(y5)),'g--')
 		
 
 	#plt.plot(bincenters3,y3,'k--')
@@ -178,7 +180,7 @@ def hist_plotter(rate_matrix_DC, rate_matrix_SA, rate_matrix_DC_BHCAP, rate_matr
 	#plt.plot(bincenters11,y11,'m-*')
 	#plt.plot(bincenters12,y12,'c:')
 	
-	plt.legend(["Dual Connectivity","Dual Connectivity + Minimum Rate"])
+	plt.legend(["Dual Connectivity","Single Association"])
 	plt.xlabel('Throughput(bps)')
 	plt.ylabel('Number of Users')
 	plt.title('Histogram of User Distribution')
@@ -186,13 +188,25 @@ def hist_plotter(rate_matrix_DC, rate_matrix_SA, rate_matrix_DC_BHCAP, rate_matr
 	plt.show()
 
 
-def scatter_plotter(rate_matrix_DC,rate_matrix_SA,np,scn):
-	plt.scatter(np.arange(1,len(rate_matrix_DC) +1),rate_matrix_DC,alpha=0.5)
-	plt.plot(np.arange(1,len(rate_matrix_DC) +1),np.ones((np.arange(1,len(rate_matrix_DC) +1).shape[0],1))*1e8, 'r--')
-	plt.xlabel('Users')
-	plt.ylabel('Throughput (in bps)')
-	plt.title("Data Rate Scatter Plot of Users (DC Scenario)")
-	plt.legend(["100 Mbps"])
+def scatter_plotter(rate_matrix_DC,rate_matrix_DC_BHCAP,np,scn):
+	
+	f,ax = plt.subplots(2)
+	f.suptitle('Data Rate Scatter Plot of Users -- DC Scenario (top); DC+BHCAP+LAT Scenario (bottom)')
+	ax[0].scatter(np.arange(1,len(rate_matrix_DC) +1),rate_matrix_DC,alpha=0.5)
+	ax[0].plot(np.arange(1,len(rate_matrix_DC) +1),np.ones((np.arange(1,len(rate_matrix_DC) +1).shape[0],1))*1e8, 'r--')
+	#ax[0].xlabel('Users')
+	#ax[0].ylabel('Throughput (in bps)')
+	#ax[0].title('a')
+	ax[0].legend(["100 Mbps"])
+	ax[1].scatter(np.arange(1,len(rate_matrix_DC_BHCAP) +1),rate_matrix_DC_BHCAP,alpha=0.5)
+	ax[1].plot(np.arange(1,len(rate_matrix_DC_BHCAP) +1),np.ones((np.arange(1,len(rate_matrix_DC_BHCAP) +1).shape[0],1))*1e8, 'r--')
+	#ax[1].xlabel('Users')
+	#ax[1].ylabel('Throughput (in bps)')
+	#ax[1].title('b')
+	ax[1].legend(["100 Mbps"])
+	f.text(0.5, 0.04, 'Users', ha='center')
+	f.text(0.04, 0.5, 'Throughput (in bps)', va='center', rotation='vertical')
+
 	plt.show()
 
 # ========================================
@@ -238,6 +252,71 @@ def accepted_user_plotter(accepted_usr_list_baseline, accepted_usr_list_SA, acce
 	ax.legend()
 	f.tight_layout()
 	plt.show()
+
+# ============================================
+# BH Utilization and Latency Provision Plotter
+# ============================================
+
+def bhutil_latprov_plotter(bhutil_val_DC, bhutil_val_DC_BHCAP, bhutil_val_DC_BHCAP_LAT, avail_bh, latprov_DC, latprov_DC_LAT, latprov_DC_MRT_LAT, latprov_DC_BHCAP_LAT, np, scn):
+
+	# ====> BH Utilization Plots
+
+	bhvbh = [item for sublist in bhutil_val_DC_BHCAP for item in sublist]
+	bhvbh_DC = [item for sublist in bhutil_val_DC for item in sublist]
+	avbh = [item for sublist in avail_bh for item in sublist]
+	#print np.amax(np.array(avbh))
+	tot_avail_bh = avbh + [scn.fib_BH_MC_capacity]*(len(bhvbh)-len(avail_bh))
+	#print len(tot_avail_bh)
+	f,axs = plt.subplots(2)
+	f.suptitle('Backhaul Resource Utilization -- Constrained (top) and Unconstrained (bottom) BH')
+	axs[0].bar(np.arange(len(bhvbh)), [x1 - x2 for (x1, x2) in zip(bhvbh, tot_avail_bh)])
+	#ax.bar(np.arange(len(bhvbh)), bhvbh)
+	#ax.plot(np.arange(len(bhvbh)), tot_avail_bh, 'r--')
+	axs[0].set_ylim(-1*(max(tot_avail_bh)+1e9), 1e9)
+	axs[0].grid()
+	#axs[0].set_title('Backhaul Resource Utilization -- Constrained BH')
+	#axs[0].set_xlabel('(a)')
+	#axs[0].set_ylabel('Demand to Available BW Difference (bps)')
+
+	axs[1].bar(np.arange(len(bhvbh_DC)), [x1 - x2 for (x1, x2) in zip(bhvbh_DC, tot_avail_bh)])
+	#ax.bar(np.arange(len(bhvbh)), bhvbh)
+	#ax.plot(np.arange(len(bhvbh)), tot_avail_bh, 'r--')
+	axs[1].set_ylim(-1*(max(tot_avail_bh)+1e9), max(tot_avail_bh)+1e9)
+	axs[1].grid()
+	#axs[1].set_title('Backhaul Resource Utilization -- Unconstrained BH')
+	#axs[1].set_xlabel('(b)')
+	#axs[1].set_ylabel('Demand to Available BW Difference (bps)')
+	f.text(0.5, 0.04, 'Access Points', ha='center')
+	f.text(0.04, 0.5, 'Demand to Available BW Difference (bps)', va='center', rotation='vertical')
+	f.tight_layout()
+	#plt.show()
+	plt.close("all")
+	# ====> Latency Plot
+	#print latprov_DC_LAT
+	lprov_DC_LAT = np.empty((latprov_DC_LAT.shape[0],2))
+	for i in range(latprov_DC_LAT.shape[0]):
+		temp = latprov_DC_LAT[i,np.nonzero(latprov_DC_LAT[i,:])]
+		if temp.shape[1] == 2:
+			lprov_DC_LAT[i,:] = temp
+		else:
+			lprov_DC_LAT[i,0] = temp
+			lprov_DC_LAT[i,1] = temp
+	#print lprov_DC_LAT[:,0].shape
+	plt.scatter(np.arange(1,lprov_DC_LAT.shape[0]+1), lprov_DC_LAT[:,0], c = 'b', marker = 'o',  alpha = 0.5)
+	plt.scatter(np.arange(1,lprov_DC_LAT.shape[0]+1), lprov_DC_LAT[:,1], c = 'b', marker = 'o',  alpha = 0.5)
+	plt.plot(np.arange(latprov_DC_LAT.shape[0]), [scn.eMBB_latency_req]*latprov_DC_LAT.shape[0],'r-')
+	plt.xlabel('Users')
+	plt.ylabel('Latency (in ms)')
+	plt.title("Latency Scatter Plot of Users (DC LAT Scenario)")
+	plt.legend(["3 ms"])
+	plt.show()
+def infeasible_iter_counter(iters_infeas, iters_infeas_DC, iters_infeas_DC_MRT, iters_infeas_DC_LAT, iters_infeas_DC_BHCAP, iters_infeas_DC_BHCAP_LAT, iters_infeas_SA_LAT, iters_infeas_SA_BHCAP, iters_infeas_SA_BHCAP_LAT, iters_infeas_SA_MRT_LAT, iters_infeas_SA_MRT ,np,scn):
+
+	plt.bar(iters_infeas_DC, iters_infeas_DC_BHCAP, iters_infeas_DC_LAT, iters_infeas_DC_MRT)
+	plt.title('Infeasible Iterations')
+	plt.show()
+
+# 
 #plt.plot(usr_lcs[0], usr_lcs[1],'k+');
 #plt.plot(macro_cell_locations[:,0], macro_cell_locations[:,1],'rs'); # Plot the macro cells
 #for j in range(0,macro_cell_locations.shape[0]):