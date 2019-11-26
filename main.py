Major Bug Fixes for the SINR calculation in BF as well as arctan function

main.py
@@ -11,6 +11,8 @@ import plotter
 import os, sys
 from scenario_var import scenario_var 
 from argparse import ArgumentParser
+import time
+import csvsaver
 
 
 # ==================================
@@ -22,16 +24,41 @@ parser.add_argument('-iter', type = int, help = 'Iteration Number of the Simulat
 parser.add_argument('-interf', type = int, help = 'Interference Limited Region Indicator')
 args = parser.parse_args(); # Parse the Arguments
 
+# ==============================
+# Initialize the class variables
+# ==============================
+scn = scenario_var(); # Getting the class object
+
 # =====================================
 # Check Presence of Storage Directories
 # =====================================
 
 path = os.getcwd() + '/Data'; # This is the path we have to check for
 subpath = os.getcwd() + '/Data/Temp'; # This is the subdirectory to store data  
+locpath = os.getcwd() + '/Data/loc'; # This is the subdirectory to store User, MC and SC location information. This should be used by any combination of Scenarios
+lcdata_flag = 1; # This flag indicates if location data exists or not
 if os.path.isdir(path):
 	print "Directory to save data found"
 	print "----------------------------"
 	print ""
+	if os.path.isdir(locpath):
+		if os.path.isfile(locpath+'/loc'+str(vars(args)['iter'])+'.npz'):
+			print "Locations Directory and Data found"
+			print "-------------------------"
+			print ""
+			lcdata_flag = 1
+		else:
+			print "Locations directory found but Data missing"
+			print "------------------------------------------"
+			print ""
+			lcdata_flag = 0
+	else:
+		os.mkdir(locpath)
+		print "Subdirectory Created"
+		print "--------------------"
+		print ""
+		lcdata_flag = 0
+
 	if os.path.isdir(subpath):
 		print "Subdirectory found"
 		print "------------------"
@@ -44,79 +71,104 @@ if os.path.isdir(path):
 else:
 	os.mkdir(path); # Create this directory 
 	os.mkdir(subpath); # Created the Subdirectory 
+	os.mkdir(locpath); #Created the User location Subdirectory
 	print "Created the Directory to save data"
 	print "----------------------------------"
 	print ""
+	lcdata_flag = 0	
 
 
-# ==============================
-# Initialize the class variables
-# ==============================
-scn = scenario_var(); # Getting the class object
+
+
 
 # ====================
 # Macro cell placement 
 # ====================
 try:
-	macro_cell_locations = scenario_gen.macro_cell(scn.simulation_area, scn.MCBS_intersite, np, dsc); # Get the macro cell locations
+	if lcdata_flag == 0:
+		macro_cell_locations = scenario_gen.macro_cell(scn.simulation_area, scn.MCBS_intersite, np, dsc); # Get the macro cell locations
 
-	#print macro_cell_locations
-	#print "===================
+		#print macro_cell_locations
+		#print "===================
 
-	# ===================================================================
-	# Small cell placement and MCBS-SCBS Additional attributes generation
-	# ===================================================================
+		# ===================================================================
+		# Small cell placement and MCBS-SCBS Additional attributes generation
+		# ===================================================================
 
-	SCBS_per_MCBS = np.random.randint(3,10,size=macro_cell_locations.shape[0]); # Randomly choosing number of SCBS within an MCBS domain in the range 3 to 1
-	SCBS_MCBS_assoc = np.zeros((sum(SCBS_per_MCBS),macro_cell_locations.shape[0]), dtype=int); # Create a MCBS and SCBS association matrix (distance based)
-	#print sum(SCBS_per_MCBS)
-	locs_SCBS = np.empty([sum(SCBS_per_MCBS),2], dtype = int); # We create an empty list of numpy arrays
-	l_idx = 0; # lower index for the association matrix 
-	u_idx = SCBS_per_MCBS[0]; # upper index for the association matrix
-	for i in range(0,macro_cell_locations.shape[0]):
-	    small_cell_locations = scenario_gen.small_cell(i, macro_cell_locations[i,:], scn.SCBS_intersite, SCBS_per_MCBS[i], scn.MCBS_intersite, np, dsc); #Get the small cell locations for each macro cell domain 
-	    #print small_cell_locations
-	    locs_SCBS[l_idx:u_idx,:] = small_cell_locations; # Store the small cell locations in the list of numpy arrays
-	    SCBS_MCBS_assoc[l_idx:u_idx,i] = dsc.dist_calc(small_cell_locations,macro_cell_locations[i], 0, 0, '2d', np); # Insert ones in these indexes for the association matrix
-	    #print SCBS_MCBS_assoc[l_idx:u_idx,i]
-	    l_idx = l_idx + SCBS_per_MCBS[i]; # Update the lower index 
-	    if i < (macro_cell_locations.shape[0]-1):
-	        u_idx = u_idx + SCBS_per_MCBS[i+1]; # Update the upper index
-	    #print locs_SCBS[:,:,i]
-	    #print "==================="
-	#print SCBS_MCBS_assoc
+		SCBS_per_MCBS = np.random.randint(3,10,size=macro_cell_locations.shape[0]); # Randomly choosing number of SCBS within an MCBS domain in the range 3 to 1
+		SCBS_MCBS_assoc = np.zeros((sum(SCBS_per_MCBS),macro_cell_locations.shape[0]), dtype=int); # Create a MCBS and SCBS association matrix (distance based)
+		#print sum(SCBS_per_MCBS)
+		locs_SCBS = np.empty([sum(SCBS_per_MCBS),2], dtype = int); # We create an empty list of numpy arrays
+		l_idx = 0; # lower index for the association matrix 
+		u_idx = SCBS_per_MCBS[0]; # upper index for the association matrix
+		for i in range(0,macro_cell_locations.shape[0]):
+		    small_cell_locations = scenario_gen.small_cell(i, macro_cell_locations[i,:], scn.SCBS_intersite, SCBS_per_MCBS[i], scn.MCBS_intersite, np, dsc); #Get the small cell locations for each macro cell domain 
+		    #print small_cell_locations
+		    locs_SCBS[l_idx:u_idx,:] = small_cell_locations; # Store the small cell locations in the list of numpy arrays
+		    SCBS_MCBS_assoc[l_idx:u_idx,i] = dsc.dist_calc(small_cell_locations,macro_cell_locations[i], 0, 0, '2d', np); # Insert ones in these indexes for the association matrix
+		    #print SCBS_MCBS_assoc[l_idx:u_idx,i]
+		    l_idx = l_idx + SCBS_per_MCBS[i]; # Update the lower index 
+		    if i < (macro_cell_locations.shape[0]-1):
+		        u_idx = u_idx + SCBS_per_MCBS[i+1]; # Update the upper index
+		    #print locs_SCBS[:,:,i]
+		    #print "==================="
+		#print SCBS_MCBS_assoc
 
-	# ========================================================
-	# Create the AP-Backhaul association for the scenario dump
-	# ========================================================
+		# ========================================================
+		# Create the AP-Backhaul association for the scenario dump
+		# ========================================================
 
-	SC_wl_bh, SC_wrd_bh, MC_hops, SC_hops = scenario_gen.backhaul_dump(scn, SCBS_per_MCBS, macro_cell_locations, SCBS_MCBS_assoc, np); # We drop the backhaul into the scenario
-	BH_capacity_SC = scenario_gen.backhaul_tput(SCBS_MCBS_assoc, SCBS_per_MCBS, SC_wl_bh, np, scn, dsc); # Also Calculate the# BH capacity vector
-	#print BH_capacity_SC 
+		SC_wl_bh, SC_wrd_bh, MC_hops, SC_hops = scenario_gen.backhaul_dump(scn, SCBS_per_MCBS, macro_cell_locations, SCBS_MCBS_assoc, np); # We drop the backhaul into the scenario
+		BH_capacity_SC = scenario_gen.backhaul_tput(SCBS_MCBS_assoc, SCBS_per_MCBS, SC_wl_bh, np, scn, dsc); # Also Calculate the# BH capacity vector
+		#print BH_capacity_SC 
 
-	# ====================================
-	# Dump the Users onto the scenario map 
-	# ====================================
+		# ====================================
+		# Dump the Users onto the scenario map 
+		# ====================================
 
-	AP_locs = np.vstack((macro_cell_locations, locs_SCBS)); # All AP locations
-	usr_locs,usr_apps_assoc = scenario_gen.user_dump(scn, SCBS_per_MCBS, macro_cell_locations.shape[0], AP_locs, np, dsc); # We also retrieve the user and applications association matrix
-	mMTC_locs = scenario_gen.mMTC_user_dump(scn,SCBS_per_MCBS,macro_cell_locations.shape[0],np); # Massive  Machine Type User locations
-	print "User and AP Dump completed"
+		AP_locs = np.vstack((macro_cell_locations, locs_SCBS)); # All AP locations
+		usr_locs,usr_apps_assoc = scenario_gen.user_dump(scn, SCBS_per_MCBS, macro_cell_locations.shape[0], AP_locs, np, dsc); # We also retrieve the user and applications association matrix
+		mMTC_locs = scenario_gen.mMTC_user_dump(scn,SCBS_per_MCBS,macro_cell_locations.shape[0],np); # Massive  Machine Type User locations
+		print usr_locs
+		print "User and AP Dump completed"
+		np.savez_compressed(os.getcwd()+'/Data/loc/loc'+str(vars(args)['iter'])+'.npz', SCBS_per_MCBS, macro_cell_locations, locs_SCBS, SC_wl_bh, SC_wrd_bh, MC_hops, SC_hops, BH_capacity_SC, SCBS_MCBS_assoc)
+		np.savez_compressed(os.getcwd()+'/Data/loc/loc_dct'+str(vars(args)['iter'])+'.npz', **usr_locs)
+		#np.savez_compressed(os.getcwd()+'/Data/loc/usles_dct'+str(vars(args)['iter'])+'.npz', **usr_apps_assoc)
+	else:
+		pass
 	# ======================================
 	# Generate the SINR values for the users
 	# ======================================
-
+	Loc_Data = np.load(os.getcwd()+'/Data/loc/loc'+str(vars(args)['iter'])+'.npz') # Get the location data 
+	usr_locs = np.load(os.getcwd()+'/Data/loc/loc_dct'+str(vars(args)['iter'])+'.npz')
+	#usr_apps_assoc = np.load(os.getcwd()+'/Data/loc/usles_dct'+str(vars(args)['iter'])+'.npz')
+	SCBS_per_MCBS = Loc_Data['arr_0']
+	macro_cell_locations = Loc_Data['arr_1']
+	locs_SCBS = Loc_Data['arr_2']
+	SC_wl_bh = Loc_Data['arr_3']
+	SC_wrd_bh = Loc_Data['arr_4']
+	MC_hops = Loc_Data['arr_5']
+	SC_hops = Loc_Data['arr_6']
+	BH_capacity_SC = Loc_Data['arr_7']
+	SCBS_MCBS_assoc = Loc_Data['arr_8']
 	#sinr_sc_embb,locs_sc_ret, usr_lcs = scenario_gen.pathloss_tester(scn, np, dsc); # Testing the Pathloss function implementation
 	for i in range(0,len(usr_locs.keys())):
 		print "Iteration #" + str(i)
 		print "====================="
 		sinr_sorted, locs_sc_ret, usr_lcs, idx_sc, idx_mc, sinr_pad, num_SCBS, num_MCBS, num_MCBS_tot, RX_eMBB, l_nl = scenario_gen.sinr_gen (scn, sum(SCBS_per_MCBS), macro_cell_locations, np.asarray(locs_SCBS), usr_locs['user_locations'+str(i)], dsc, np, int(vars(args)['interf']) )
+		#print sinr_sorted
+		csvsaver.csvsaver(sinr_sorted,[],"SINR_rx1.csv")
+		
+		#print sinr_sorted.shape 
 		sinr = dsc.reorganizer(sinr_sorted, idx_sc, idx_mc, num_SCBS, num_MCBS_tot, sinr_pad, np, scn); # We reorganize the SINR matrix for the optimization framework
+		#print sinr
+		csvsaver.csvsaver(sinr,[],"SINR_REOG.csv")
+		#print sinr.shape 
 		RX = dsc.reorganizer(RX_eMBB, idx_sc, idx_mc, num_SCBS, num_MCBS_tot, float('nan'), np, scn); # We reorganize the RX Power matrix for the Baseline framework
 		# ================================
 		# Create Compressed Variable Files
 		
-		np.savez_compressed(os.getcwd()+'/Data/Temp/optim_var_'+ str(i) + str(vars(args)['iter']),sinr, usr_apps_assoc, usr_lcs, idx_sc, sinr_pad, num_SCBS, num_MCBS_tot, SC_wl_bh, SC_wrd_bh, MC_hops, SC_hops, BH_capacity_SC, RX, SCBS_per_MCBS, l_nl, allow_pickle = True); # Save these variables to be utilized by the optimizer
+		np.savez_compressed(os.getcwd()+'/Data/Temp/optim_var_'+ str(i) + str(vars(args)['iter']),sinr, usr_lcs, idx_sc, sinr_pad, num_SCBS, num_MCBS_tot, SC_wl_bh, SC_wrd_bh, MC_hops, SC_hops, BH_capacity_SC, RX, SCBS_per_MCBS, l_nl, allow_pickle = True); # Save these variables to be utilized by the optimizer
 		np.savez_compressed(os.getcwd()+'/Data/Temp/hmap_' + str(i) + str(vars(args)['iter']), usr_lcs, locs_SCBS, macro_cell_locations, SCBS_per_MCBS, SCBS_MCBS_assoc, l_nl, sinr) # Data necessary for heatmap is saved here
 		#np.savez_compressed('/home/akshayjain/Desktop/Simulation/optim_var_1',sinr_sorted, usr_apps_assoc, usr_lcs, idx_sc, sinr_pad, num_SCBS, num_MCBS, SC_wl_bh, SC_wrd_bh, MC_hops, SC_hops, BH_capacity_SC); # Save these variables to be utilized by the optimizer
 

optimizer_func.py
@@ -86,24 +86,24 @@ for N in range(0,num_iter):
 	print "Dataset # " + str(N) + ": Collecting the Stored Variables"
 
 	optim_data = np.load(os.getcwd() + '/Data/Temp/optim_var_'+ str(N) + str(vars(args)['iter']) +'.npz', allow_pickle = True)
-	sinr_APs = optim_data['arr_0']; # Load the SINR data to be used for the optimization
-	user_AP_assoc = optim_data['arr_1'].item()['user_app' + str(N)]; # Load the User-Applications Association data to be used for the optimization
-	sinr_eMBB = np.empty([np.sum(user_AP_assoc[:,1]),sinr_APs.shape[1]],dtype=float); # Array that holds the Application SINR values
-	sinr_pad_val = optim_data['arr_4']; # In the small cell calculation we use an sinr pad value for ease of computation
-	num_scbs = optim_data['arr_5']; # Number of Small cells
-	num_mcbs = optim_data['arr_6']; # Number of Macro cells
-	mat_wlbh_sc = optim_data['arr_7']; # Wireless backhaul matrix for Small Cells
-	mat_wrdbh_sc = optim_data['arr_8']; # Wired backhaul matrix for Macro cells
-	Hops_MC = optim_data['arr_9']; # Number of hops to the IMS core from Macro cells
-	Hops_SC = optim_data['arr_10']; # Number of hops to the IMS core from Small cells
-	BH_Capacity_SC = optim_data['arr_11']; # Backhaul capacity for Small cells
+	sinr_eMBB = optim_data['arr_0']; # Load the SINR data to be used for the optimization
+	#user_AP_assoc = optim_data['arr_1'].item()['user_app' + str(N)]; # Load the User-Applications Association data to be used for the optimization
+	#sinr_eMBB = np.empty([np.sum(user_AP_assoc[:,1]),sinr_APs.shape[1]],dtype=float); # Array that holds the Application SINR values
+	sinr_pad_val = optim_data['arr_3']; # In the small cell calculation we use an sinr pad value for ease of computation
+	num_scbs = optim_data['arr_4']; # Number of Small cells
+	num_mcbs = optim_data['arr_5']; # Number of Macro cells
+	mat_wlbh_sc = optim_data['arr_6']; # Wireless backhaul matrix for Small Cells
+	mat_wrdbh_sc = optim_data['arr_7']; # Wired backhaul matrix for Macro cells
+	Hops_MC = optim_data['arr_8']; # Number of hops to the IMS core from Macro cells
+	Hops_SC = optim_data['arr_9']; # Number of hops to the IMS core from Small cells
+	BH_Capacity_SC = optim_data['arr_10']; # Backhaul capacity for Small cells
 	BH_Capacity_MC = scn.fib_BH_MC_capacity; # Backhaul capacity for Macro cells
-	SNR_iter = optim_data['arr_12']; # Small Cell Received Power 
-	SCBS_per_MCBS = optim_data['arr_13']; # Number of small cells per macro cell
+	SNR_eMBB = optim_data['arr_11']; # Small Cell Received Power 
+	SCBS_per_MCBS = optim_data['arr_12']; # Number of small cells per macro cell
 	
 	#RX_power_mc = optim_data['arr_13']; # Macro Cell Received Power 
 	#RX_power = np.hstack((RX_power_mc,RX_power_sc)); # Stack all the received powers for the eMBB users
-	SNR_eMBB = np.empty([np.sum(user_AP_assoc[:,1]),sinr_APs.shape[1]],dtype=float); # Array that holds the Application SINR values
+	#SNR_eMBB = np.empty([np.sum(user_AP_assoc[:,1]),sinr_APs.shape[1]],dtype=float); # Array that holds the Application SINR values
 	# ==================================
 	# Print to Understand Matrix Formats
 	#print num_scbs
@@ -127,13 +127,16 @@ for N in range(0,num_iter):
 
 	print "Creating the Application to Access Point SINR association matrix"
 	iter = 0; # Application number tracking
-	for i in range(0,sinr_APs.shape[0]):
-		sinr_eMBB [iter:iter + np.sum(user_AP_assoc[i,1]), :] = np.delete(np.outer(user_AP_assoc[i,1],sinr_APs[i,:]), np.where(user_AP_assoc[i,1] == 0), 0);# Application to Base Station SINR matrix 
-	 	SNR_eMBB[iter:iter + np.sum(user_AP_assoc[i,1]), :] = np.delete(np.outer(user_AP_assoc[i,1],SNR_iter[i,:]), np.where(user_AP_assoc[i,1] == 0), 0);# Application to Base Station SINR matrix 
-	 	iter = iter + np.sum(user_AP_assoc[i,1]); # Incrementing the iterator for the next user-application sets
+	# for i in range(0,sinr_APs.shape[0]):
+	# 	#sinr_eMBB [iter:iter + np.sum(user_AP_assoc[i,1]), :] = np.delete(np.outer(user_AP_assoc[i,1],sinr_APs[i,:]), np.where(user_AP_assoc[i,1] == 0), 0);# Application to Base Station SINR matrix 
+	#  	SNR_eMBB[iter:iter + np.sum(user_AP_assoc[i,1]), :] = np.delete(np.outer(user_AP_assoc[i,1],SNR_iter[i,:]), np.where(user_AP_assoc[i,1] == 0), 0);# Application to Base Station SINR matrix 
+	#  	iter = iter + np.sum(user_AP_assoc[i,1]); # Incrementing the iterator for the next user-application sets
 	
+	#sinr_eMBB = sinr_APs
+	#SNR_eMBB = SNR_iter
 	#np.savetxt('SINR.csv',sinr_eMBB, delimiter=",")
-	csvsaver.csvsaver(np.where(sinr_eMBB == sinr_pad_val, 0, sinr_eMBB), [], "SINR.csv")
+	print sinr_eMBB
+	csvsaver.csvsaver(sinr_eMBB, [], "SINR"+str(N)+".csv")
 	#print sinr_eMBB
 	#print sinr_applications[0]
 
@@ -150,7 +153,7 @@ for N in range(0,num_iter):
 			rate[:,i] = np.where(sinr_eMBB[:,i] == sinr_pad_val, 0, np.log2(1 + 10**(sinr_eMBB[:,i]/10))); # Rate calculation for MC  
 
 	var_row_num = sinr_eMBB.shape[0];
-	var_col_num = sinr_APs.shape[1];
+	var_col_num = sinr_eMBB.shape[1];
 
 	#print var_row_num
 
@@ -510,7 +513,7 @@ for N in range(0,num_iter):
 					Rate_data[i,1] = np.sum(new_rate[i, num_scbs + np.nonzero(new_rate[i,num_scbs:num_scbs+num_mcbs])])
 				#print np.where(new_rate[i, num_scbs + np.nonzero(new_rate[i,num_scbs:num_scbs+num_mcbs])].size == 0 , "Size is 0", new_rate[i, num_scbs + np.nonzero(new_rate[i,num_scbs:num_scbs+num_mcbs])]) 
 
-			csvsaver.csvsaver(Rate_data,["SC Rate","MC Rate"], "OptimizedDataRate_user_MCSC.csv")
+			#csvsaver.csvsaver(Rate_data,["SC Rate","MC Rate"], "OptimizedDataRate_user_MCSC.csv")
 			#print np.sum(new_rate, axis = 1)
 			print np.amin(np.sum(new_rate,axis  = 1))
 			print np.amax(np.sum(new_rate,axis  = 1))
@@ -532,14 +535,14 @@ for N in range(0,num_iter):
 			csvsaver.csvsaver(G_sum,["Accepted Users per SC"], "GS.csv")
 			csvsaver.csvsaver(M_sum,["Accepted Users per MC"], "MC.csv")			
 
-			if N == (num_iter-1) and (vars(args)['dual'] == 1 or vars(args)['bhaul'] == 1 or vars(args)['minRate'] == 1 or vars(args)['latency'] == 1):
-				#plotter.optimizer_plotter(new_rate) # We get the plot for the rates with maximum number of users
-				with open("Rate" + str(vars(args)['iter']) + str(vars(args)['dual']) + str(vars(args)['bhaul']) + str(vars(args)['minRate']) + str(vars(args)['latency']) + ".csv", "w+") as my_csv:
-					csvWriter = csv.writer(my_csv,delimiter=',')
-					csvWriter.writerows(new_rate) # We write the rate matrix to the csv file for visualization
-				with open("OptAssignment" + str(vars(args)['iter']) + str(vars(args)['dual']) + str(vars(args)['bhaul']) + str(vars(args)['minRate']) + str(vars(args)['latency']) + ".csv", "w+") as my_csv2:
-					csvWriter = csv.writer(my_csv2,delimiter=',')
-					csvWriter.writerows(np.asarray(X_optimal).reshape((var_row_num,var_col_num))) # We write the optimal association matrix to csv files for analysis purposes
+			# if N == (num_iter-1) and (vars(args)['dual'] == 1 or vars(args)['bhaul'] == 1 or vars(args)['minRate'] == 1 or vars(args)['latency'] == 1):
+			# 	#plotter.optimizer_plotter(new_rate) # We get the plot for the rates with maximum number of users
+			# 	with open("Rate" + str(vars(args)['iter']) + str(vars(args)['dual']) + str(vars(args)['bhaul']) + str(vars(args)['minRate']) + str(vars(args)['latency']) + ".csv", "w+") as my_csv:
+			# 		csvWriter = csv.writer(my_csv,delimiter=',')
+			# 		csvWriter.writerows(new_rate) # We write the rate matrix to the csv file for visualization
+			# 	with open("OptAssignment" + str(vars(args)['iter']) + str(vars(args)['dual']) + str(vars(args)['bhaul']) + str(vars(args)['minRate']) + str(vars(args)['latency']) + ".csv", "w+") as my_csv2:
+			# 		csvWriter = csv.writer(my_csv2,delimiter=',')
+			# 		csvWriter.writerows(np.asarray(X_optimal).reshape((var_row_num,var_col_num))) # We write the optimal association matrix to csv files for analysis purposes
 			#plotter.optimizer_plotter(M_plt_idx[:,:,0] + M_plt_idx[:,:,1] + M_plt_idx[:,:,2] + M_plt_idx[:,:,3] + M_plt_idx[:,:,4])0
 			#plotter.optimizer_plotter(G_plt_idx[:,:,0] + G_plt_idx[:,:,1] + G_plt_idx[:,:,2])	
 
@@ -557,7 +560,7 @@ for N in range(0,num_iter):
 			Data['Apps'+str(N)] = var_row_num;
 			Data['APs'+str(N)] = var_col_num;
 			Data['Time'+str(N)] = m.Runtime;
-			Data['SINR'+str(N)] = sinr_eMBB; 
+			Data['SINR'+str(N)] = sinr_eMBB;
 
 			#print np.sum((G_total_compute>0)*1, axis = 1) 
 			#print np.sum((np.asarray(X_optimal).reshape((var_row_num,var_col_num))>0)*1, axis =1)

scenario_gen.py
@@ -10,6 +10,7 @@ import copy
 import csv
 import csvsaver
 import sectbeam
+import pdb
 #from multiprocessing import Process
 # ===================================================
 # Load/Generate the Macro Cell base station locations
@@ -199,6 +200,7 @@ def sinr_gen (scn, num_SCBS, mc_locs, sc_locs, usr_lcs, dsc, np, inter_limit_fla
     # ======================================================
     # Limit the number of MC and SC for the SINR calculation
 
+    #pdb.set_trace()
     
     # ==> eMBB users
     if inter_limit_flag == 1: # Interference limited scenario with no sectoring employed
@@ -207,7 +209,7 @@ def sinr_gen (scn, num_SCBS, mc_locs, sc_locs, usr_lcs, dsc, np, inter_limit_fla
         print "================================================="
 
         num_MCBS_SINR = 4; # We choose the 4 closest MCs for the SINR calculation 
-        dist_SCBS_SINR = 200; # We choose the range of the farthest SC that will impact SINR calculation for a user to be 200 meters
+        dist_SCBS_SINR = 100; # We choose the range of the farthest SC that will impact SINR calculation for a user to be 200 meters
         sorted_MCBS_mat, idx_MCBS_SINR = dsc.idx_mat(dist_serv_cell, num_MCBS_SINR,'minimum',np); # Distance based sorted matrix and index of the MCBS under consideration for the PL calculation
         sorted_SCBS_mat, idx_SCBS_SINR = dsc.idx_mat(dist_serv_sc, dist_SCBS_SINR, 'distance', np); # Distance based sorted matrix and index of the SCBS under consideration for the PL calculation
         #print sorted_MCBS_mat.shape
@@ -256,7 +258,7 @@ def sinr_gen (scn, num_SCBS, mc_locs, sc_locs, usr_lcs, dsc, np, inter_limit_fla
         print "Performing Interference Calculation"
         interf_sc = dsc.interf(PL_sc, scn, np, scn.transmit_power, scn.transmit_gain_sc); # Calculate the interference matrix for small cells
         interf_mc = dsc.interf(PL_mc, scn, np, scn.max_tnsmtpow_MCBS, scn.ant_gain_MCBS); # Calculate the interference matrix for macro cells. MCs and SCs work on different frequency bands and hence do not interfere with each other
-        
+        csvsaver.csvsaver(interf_sc,[],"InterferenceSC.csv")
         #print interf_sc[1,:]
 
         # ====================
@@ -293,6 +295,7 @@ def sinr_gen (scn, num_SCBS, mc_locs, sc_locs, usr_lcs, dsc, np, inter_limit_fla
             #sinr_sc[i, np.where(np.isnan(sinr_sc[i,:]) == True )] = np.amin(np.where(np.isnan(sinr_sc[i,:]) != True )); # Replace the None values with the minimum of that row 
             #print sinr_sc[i,:]
       
+        csvsaver.csvsaver(sinr_sc,[],"SINR_SC.csv")
         print "Performing SINR Calculation for Macro cells"
 
         sinr_mc = np.empty((sorted_MCBS_mat.shape[0], sorted_MCBS_mat.shape[1])); # Initialize SINR matrix
@@ -313,10 +316,12 @@ def sinr_gen (scn, num_SCBS, mc_locs, sc_locs, usr_lcs, dsc, np, inter_limit_fla
         
         
         num_MCBS_SINR = 4; # We choose the 4 closest MCs for the SINR calculation 
-        dist_SCBS_SINR = 200; # We choose the range of the farthest SC that will impact SINR calculation for a user to be 200 meters
+        dist_SCBS_SINR = 100; # We choose the range of the farthest SC that will impact SINR calculation for a user to be 200 meters
         sorted_MCBS_mat, idx_MCBS_SINR = dsc.idx_mat(dist_serv_cell, num_MCBS_SINR,'minimum',np); # Distance based sorted matrix and index of the MCBS under consideration for the PL calculation
         sorted_SCBS_mat, idx_SCBS_SINR = dsc.idx_mat(dist_serv_sc, dist_SCBS_SINR, 'distance', np); # Distance based sorted matrix and index of the SCBS under consideration for the PL calculation
 
+        #print idx_SCBS_SINR
+        #print sorted_MCBS_mat.shape
         # ==> For Macro Cell
 
         print "\n Sectorizing Macro Cells and Computing Interferers"
@@ -328,11 +333,16 @@ def sinr_gen (scn, num_SCBS, mc_locs, sc_locs, usr_lcs, dsc, np, inter_limit_fla
         
         PL_mc = np.empty((sorted_MCBS_mat.shape[0], sorted_MCBS_mat.shape[1])); # Initializing the Pathloss matrix
         interf_mc = np.zeros((sorted_MCBS_mat.shape[0], sorted_MCBS_mat.shape[1])); # This is the matrix that will hold the interference values for each UE and significant TXs    
-        
+        #interf_sect_data = np.zeros((sorted_MCBS_mat.shape[0], sorted_MCBS_mat.shape[1]), dtype = object)
         for i in range(sorted_MCBS_mat.shape[0]):
             interf_sect = [] # An empty list of indices which will store the list of other interfering cells
             for j in range(sorted_MCBS_mat.shape[1]):
                 interf_sect = np.where(sector_UEMCBS[i,:] == sector_UEMCBS[i,idx_MCBS_SINR[i,j]])[0] # The interfering cells
+                #print ("SOmething Else:", np.where(sector_UEMCBS[i,:] == sector_UEMCBS[i,idx_MCBS_SINR[i,j]]))
+                # print ("MCBS of Interest:", idx_MCBS_SINR[i,j])
+                # print ("Sector of MCBS of Interest:", sector_UEMCBS[i,idx_MCBS_SINR[i,j]])
+                # print ("Interfering APs:", interf_sect)
+                #interf_sect_data[i,j] = interf_sect
                 #PL_mc[i,j], l_nl[i, j+ num_SCBS] = pathloss.pathloss_CI(scn, sorted_MCBS_mat[i][j], np, dist_serv_cell_3d[i][j], dsc, 0); # Calculating the pathloss for Macro cells
                 PL_mc[i,j], l_nl[i, j+ num_SCBS] = pathloss.pathloss_CI(scn, sorted_MCBS_mat[i][j], np, dist_serv_cell_3d[i][int(idx_MCBS_SINR[i,j])], dsc, 0); # Calculating the pathloss for Macro cells
                 temp = np.empty((len(interf_sect)-1)); # An empty numpy array to hold the pathloss of interfereing cells
@@ -342,13 +352,14 @@ def sinr_gen (scn, num_SCBS, mc_locs, sc_locs, usr_lcs, dsc, np, inter_limit_fla
                 for k in range(len(interf_sect)):
                     #print interf_sect[k]
                     if interf_sect[k] != idx_MCBS_SINR[i,j]:
+                        #print ("Interference Calculation using indexes:", interf_sect[k])
                         temp[idx_temp], dummy = pathloss.pathloss_CI(scn, dist_serv_cell[i][interf_sect[k]], np, dist_serv_cell_3d[i][interf_sect[k]], dsc, 0); # Calculate the pathloss from the similar sector antennas to the UE
                         idx_temp = idx_temp + 1; # Increment the temp vector index
                         #print temp
                 interf_mc[i,j] =  np.sum((10**(scn.max_tnsmtpow_MCBS/10)*(10**(scn.ant_gain_MCBS/10))*(10**(scn.receiver_gain/10)*10**(-3)))/(10**(temp/10))); # Interference for User i and AP j
         
         print "Performing SINR Calculation for Macro cells"
-
+        #csvsaver.csvsaver(interf_sect_data,[],'Interfering Sectors Data')
         sinr_mc = np.empty((sorted_MCBS_mat.shape[0], sorted_MCBS_mat.shape[1])); # Initialize SINR matrix
         for i in range(0,PL_mc.shape[0]):
             for j in range(0, PL_mc.shape[1]):
@@ -396,20 +407,28 @@ def sinr_gen (scn, num_SCBS, mc_locs, sc_locs, usr_lcs, dsc, np, inter_limit_fla
                 #print idx_SCBS_SINR[i,j]
                 #rint usr_lcs[i,:]
                 if idx_SCBS_SINR[i,j] != 'None':
+                    #print "Here"
                     glob_angle_sc_rx[i,j] = dsc.angsc(usr_lcs[i,:],sc_locs[int(idx_SCBS_SINR[i,j]),:],np,scn) # Angle calculator to determine if 
                 else:
-                    glob_angle_sc_rx[i,j] = float('nan') # Nan for the APs beyond 200m radius
-
+                    glob_angle_sc_rx[i,j] = float('Nan') # Nan for the APs beyond 200m radius
+        #print glob_angle_sc_rx
+        csvsaver.csvsaver(usr_lcs,[],"UELOCS.csv")
+        csvsaver.csvsaver(glob_angle_sc_rx,[],"SCAngles.csv")
+        csvsaver.csvsaver(sc_locs,[],"SCLocs.csv")
+        csvsaver.csvsaver(idx_SCBS_SINR,[],"SelectSCIDX.csv")
+        csvsaver.csvsaver(PL_sc,[],"PL_sc.csv")
         print "Common Sector and Average Interference Computation"
 
         for i in range(sorted_SCBS_mat.shape[0]):
             for j in range(sorted_SCBS_mat.shape[1]):
                 ap_int_idx = j; # This is our AP of interest
                 interf_ap_idx = np.where(glob_angle_sc_rx[i,:] == glob_angle_sc_rx[i,ap_int_idx])[0] # These are the indexes of the APs that will be interfering with the AP of interest
+                #print interf_ap_idx
                 #interf_sc[i,ap_int_idx] = np.sum((scn.beam_hpbw_tx/(360))*PL_sc[i,interf_ap_idx]) 
                 # (10**(tx_power/10)*(10**(gain/10))*(10**(scn.receiver_gain/10)*10**(-3)))/(10**(PL_temp/10))
-                interf_sc[i,ap_int_idx] = np.sum((10**(scn.transmit_power/10)*(10**(scn.transmit_gain_sc/10))*(10**(scn.receiver_gain/10)*10**(-3)))/(10**(PL_sc[i,interf_ap_idx]/10))) # We just use the calculated PL
-
+                #print PL_sc[i,interf_ap_idx]
+                interf_sc[i,ap_int_idx] = np.sum((10**(scn.transmit_power/10)*(10**(scn.transmit_gain_sc/10))*(10**(scn.receiver_gain/10)*10**(-3)))/(10**(PL_sc[i,interf_ap_idx]/10))) - (10**(scn.transmit_power/10)*(10**(scn.transmit_gain_sc/10))*(10**(scn.receiver_gain/10)*10**(-3)))/(10**(PL_sc[i,ap_int_idx]/10)) # We just use the calculated PL
+        csvsaver.csvsaver(interf_sc,[],"InterferenceSC.csv")
 
         # ===> We try the SNR regime (Best Case solution with extreme directivity)
 
@@ -428,7 +447,9 @@ def sinr_gen (scn, num_SCBS, mc_locs, sc_locs, usr_lcs, dsc, np, inter_limit_fla
             #sinr_sc[i, np.where(np.isnan(sinr_sc[i,:]) == True )] = np.amin(np.where(np.isnan(sinr_sc[i,:]) != True )); # Replace the None values with the minimum of that row 
             #print sinr_sc[i,:] 
 
-
+        csvsaver.csvsaver(sinr_sc,[],"SINR_SC.csv")
+        #print sinr_sc.shape 
+        #print sinr_mc.shape 
         # ====================
         # Rx Power Computation
 